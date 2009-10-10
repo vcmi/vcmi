c4e82959f1b523d5ad03d9b37825c1349d643cbd
Frank Zago <tatou99@gmail.com>
2009-10-10 15:09:02 +0000

    Added file missing from previous commit.

---
+++ lib/VCMIDirs.h
@@ -0,0 +1,35 @@
+/*
+ * UserHome.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
+#include <boost/filesystem.hpp>
+
+using namespace boost::filesystem;
+
+/* Where to find the various VCMI files. This is mostly usefull for linux. */
+class VCMIDirs {
+public:
+	std::string UserPath;
+
+	VCMIDirs()
+	{
+#ifdef _WIN32
+		UserPath = DATA_DIR;
+#else
+		// Find vcmi user directory and create it if necessary
+		std::string home_dir = getenv("HOME");
+		UserPath = path(home_dir + "/.vcmi").string();
+
+		create_directory(UserPath);
+		create_directory(UserPath + "/config");
+		create_directory(UserPath + "/Games");
+#endif
+	}
+};
+extern VCMIDirs GVCMIDirs;
