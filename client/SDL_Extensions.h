921b5e0c0919109100bd73149e3fd4250a659907
mateuszb <mateuszbaran89@gmail.com>
2010-02-18 12:44:59 +0000

    * missing change

--- client/SDL_Extensions.h
+++ client/SDL_Extensions.h
@@ -100,6 +100,7 @@ namespace CSDL_Ext
 	Uint32 SDL_GetPixel(SDL_Surface *surface, const int & x, const int & y, bool colorByte = false);
 	SDL_Color SDL_GetPixelColor(SDL_Surface *surface, int x, int y); //returns color of pixel at given position
 	void alphaTransform(SDL_Surface * src); //adds transparency and shadows (partial handling only; see examples of using for details)
+	bool isTransparent(SDL_Surface * srf, int x, int y); //checks if surface is transparent at given position
 
 	void blitWithRotate1(const SDL_Surface *src, const SDL_Rect * srcRect, SDL_Surface * dst, const SDL_Rect * dstRect);//srcRect is not used, works with 8bpp sources and 24bpp dests
 	void blitWithRotate2(const SDL_Surface *src, const SDL_Rect * srcRect, SDL_Surface * dst, const SDL_Rect * dstRect);//srcRect is not used, works with 8bpp sources and 24bpp dests
