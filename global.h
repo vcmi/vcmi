d8edfbf38a3c8e28a3d1df7c1cfcbbbf9df9e7e5
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2010-02-26 11:25:45 +0000

    Huh... It needs to be 0.75e

--- global.h
+++ global.h
@@ -20,7 +20,7 @@ typedef boost::int8_t si8; //signed int 8 bits (1 byte)
 #define THC
 #endif
 
-#define NAME_VER ("VCMI 0.75d")
+#define NAME_VER ("VCMI 0.75e")
 extern std::string NAME; //full name
 extern std::string NAME_AFFIX; //client / server
 #define CONSOLE_LOGGING_LEVEL 5
