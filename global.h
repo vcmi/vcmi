c551a32f9f55d44e0dafb6f43127530c2ed8d49a
DjWarmonger <warmonger@vp.pl>
2011-07-21 15:03:58 +0000

    Version bump.

--- global.h
+++ global.h
@@ -38,7 +38,7 @@ typedef si32 TBonusSubtype;
 #define THC
 #endif
 
-#define NAME_VER ("VCMI 0.85b")
+#define NAME_VER ("VCMI 0.85c")
 extern std::string NAME; //full name
 extern std::string NAME_AFFIX; //client / server
 #define CONSOLE_LOGGING_LEVEL 5
