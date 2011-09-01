29eb3d10ec778cf805cc9b51939c8b6615571790
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2011-09-01 06:51:44 +0000

    Version set to 0.86.

--- global.h
+++ global.h
@@ -38,7 +38,7 @@ typedef si32 TBonusSubtype;
 #define THC
 #endif
 
-#define NAME_VER ("VCMI 0.85c")
+#define NAME_VER ("VCMI 0.86")
 extern std::string NAME; //full name
 extern std::string NAME_AFFIX; //client / server
 #define CONSOLE_LOGGING_LEVEL 5
