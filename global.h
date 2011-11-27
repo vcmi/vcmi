e02ccb24a50fdd14cf5525d481c2ea33c9246ee4
DjWarmonger <warmonger@vp.pl>
2011-11-27 08:57:45 +0000

    Version set to 0.86e

--- global.h
+++ global.h
@@ -38,7 +38,7 @@ typedef si32 TBonusSubtype;
 #define THC
 #endif
 
-#define NAME_VER ("VCMI 0.86d")
+#define NAME_VER ("VCMI 0.86e")
 extern std::string NAME; //full name
 extern std::string NAME_AFFIX; //client / server
 #define CONSOLE_LOGGING_LEVEL 5
