7087be1f02be293ed56dad49961be0cd9a35027b
alexvins <alexvins@users.noreply.github.com>
2012-09-15 18:44:55 +0000

    C::B project files initial import + workspace file (not include ERM and Battle AI) + project files * svn:ignore setup

--- AI/EmptyAI/exp_funcs.cpp
+++ AI/EmptyAI/exp_funcs.cpp
@@ -3,27 +3,27 @@
 #include "CEmptyAI.h"
 
 std::set<CGlobalAI*> ais;
-extern "C" DLL_LINKAGE int GetGlobalAiVersion()
+extern "C" DLL_EXPORT int GetGlobalAiVersion()
 {
 	return AI_INTERFACE_VER;
 }
 
-extern "C" DLL_LINKAGE void GetAiName(char* name)
+extern "C" DLL_EXPORT void GetAiName(char* name)
 {
 	strcpy(name,NAME);
 }
-extern "C" DLL_LINKAGE char * GetAiNameS()
+extern "C" DLL_EXPORT char * GetAiNameS()
 {
 	char * ret = new char[50];
 	strcpy(ret,NAME);
 	return ret;
 }
-extern "C" DLL_LINKAGE CGlobalAI * GetNewAI()
+extern "C" DLL_EXPORT CGlobalAI * GetNewAI()
 {
 	return new CEmptyAI();
 // return
 }
-extern "C" DLL_LINKAGE void ReleaseAI(CGlobalAI * i)
+extern "C" DLL_EXPORT void ReleaseAI(CGlobalAI * i)
 {
 	delete (CEmptyAI*)i;
 	ais.erase(i);
