2f1cf09881f19cf40d8572ab5a509c0c960d3051
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2013-07-07 08:30:06 +0000

    VS project for Tests, not really working ATM though.

--- test/StdInc.cpp
+++ test/StdInc.cpp
@@ -1,7 +1,2 @@
 // Creates the precompiled header
 #include "StdInc.h"
-
-#define BOOST_TEST_MODULE VcmiTest
-#include <boost/test/unit_test.hpp>
-#include "CVcmiTestConfig.h"
-BOOST_GLOBAL_FIXTURE(CVcmiTestConfig);
--- test/StdInc.h
+++ test/StdInc.h
@@ -2,7 +2,13 @@
 
 #include "../Global.h"
 
-#define BOOST_TEST_DYN_LINK
+#ifndef _MSC_VER
+	#define BOOST_TEST_DYN_LINK
+#endif
 
+#define BOOST_TEST_MODULE VcmiTest
+#include <boost/test/unit_test.hpp>
+#include "CVcmiTestConfig.h"
+BOOST_GLOBAL_FIXTURE(CVcmiTestConfig);
 
 
