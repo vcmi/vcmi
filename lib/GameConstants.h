4a9194a6445f1e194b4ee52c6e2d7ed92159aa8e
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2013-10-01 18:44:44 +0000

    Version set to 0.94. Updated changelog.

--- lib/GameConstants.h
+++ lib/GameConstants.h
@@ -14,7 +14,7 @@
 
 namespace GameConstants
 {
-	const std::string VCMI_VERSION = "VCMI 0.93e";
+	const std::string VCMI_VERSION = "VCMI 0.94";
 
 	const int BFIELD_WIDTH = 17;
 	const int BFIELD_HEIGHT = 11;
