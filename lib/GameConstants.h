f5e3e578e64f146425ba889d9a5ce221fc2b89ac
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2014-02-09 22:44:57 +0000

    Version bump 0.94e.

--- lib/GameConstants.h
+++ lib/GameConstants.h
@@ -14,7 +14,7 @@
 
 namespace GameConstants
 {
-	const std::string VCMI_VERSION = "VCMI 0.94d";
+	const std::string VCMI_VERSION = "VCMI 0.94e";
 
 	const int BFIELD_WIDTH = 17;
 	const int BFIELD_HEIGHT = 11;
