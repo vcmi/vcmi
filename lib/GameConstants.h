6d73052ca1aabc60ac0f0f4c3c654542548412c1
Michał W. Urbańczyk <mwu-tow@gazeta.pl>
2014-02-23 23:45:58 +0000

    Development release 0.94f

--- lib/GameConstants.h
+++ lib/GameConstants.h
@@ -14,7 +14,7 @@
 
 namespace GameConstants
 {
-	const std::string VCMI_VERSION = "VCMI 0.94e";
+	const std::string VCMI_VERSION = "VCMI 0.94f";
 
 	const int BFIELD_WIDTH = 17;
 	const int BFIELD_HEIGHT = 11;
