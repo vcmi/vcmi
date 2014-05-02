9996013946a5f054aa38dc8c3bf3b188039bb046
stopiccot <alexey.petruchik@gmail.com>
2014-05-02 16:04:12 +0300

    changes xcode-pack url

--- osx/download_dependencies.sh
+++ osx/download_dependencies.sh
@@ -2,7 +2,7 @@ if [ -f ../.osx_dependencies_installed ];
 then
    echo "OS X prebuilt dependencies are already installled"
 else
-	curl -o ../xcode-pack.zip -L https://dl.dropboxusercontent.com/u/1777581/xcode-pack.zip
+	curl -o ../xcode-pack.zip -L http://download.vcmi.eu/xcode-pack.zip
 	unzip ../xcode-pack.zip -d ../
 	touch ../.osx_dependencies_installed
 fi
