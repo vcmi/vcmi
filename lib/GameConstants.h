59ab30751c61ed7f4781f923cd7d527f3971ddb5
DjWarmonger <Warmonger@vp.pl>
2014-09-24 18:25:28 +0200

    - Version set to 0.96b - Updated changelog

--- lib/GameConstants.h
+++ lib/GameConstants.h
@@ -14,7 +14,7 @@
 
 namespace GameConstants
 {
-	const std::string VCMI_VERSION = "VCMI 0.96";
+	const std::string VCMI_VERSION = "VCMI 0.96b";
 
 	const int BFIELD_WIDTH = 17;
 	const int BFIELD_HEIGHT = 11;
