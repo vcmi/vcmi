f5f0f61076fec46bd4fa27cbc3fa2dea7882bf0f
DjWarmonger <Warmonger@vp.pl>
2014-12-26 17:13:41 +0100

    Density does not depend on zone size.

--- lib/rmg/CRmgTemplateZone.cpp
+++ lib/rmg/CRmgTemplateZone.cpp
@@ -1267,8 +1267,7 @@ void CRmgTemplateZone::createTreasures(CMapGenerator* gen)
 		//also, normalize it to zone count - higher count means relatively smaller zones
 
 		//this is squared distance for optimization purposes
-		const double minDistance = std::max<float>((300.f * size * size * gen->getZones().size()) /
-			(gen->mapGenOptions->getWidth() * gen->mapGenOptions->getHeight() * totalDensity * (gen->map->twoLevel ? 2 : 1)), 2);
+		const double minDistance = std::max<float>((200.f / totalDensity), 2);
 		//distance lower than 2 causes objects to overlap and crash
 
 		do {
