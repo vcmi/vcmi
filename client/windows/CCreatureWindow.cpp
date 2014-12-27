vcmi/vcmi

commit 8a697f2ba3fe37ea225050e1f9cf5bc71cb1362b

Fix disabling of dismiss button for last troop
Fix issue 1994

client/windows/CCreatureWindow.cpp
@@ -528,7 +528,7 @@ void CStackWindow::CWindowSection::createButtonPanel()
 	OBJ_CONSTRUCTION_CAPTURING_ALL;
 	createBackground("button-panel");
 
-	if (parent->info->dismissInfo)
+	if (parent->info->dismissInfo->callback)
 	{
 		auto onDismiss = [=]()
 		{

commit ceb5da44d104b86f0ad17938425faa4ce117e35c

This fixes crash in battle I introduced in 8a697f2 dismissInfo do not present when createButtonPanel used inside the battle.

client/windows/CCreatureWindow.cpp
@@ -528,7 +528,7 @@ void CStackWindow::CWindowSection::createButtonPanel()
 	OBJ_CONSTRUCTION_CAPTURING_ALL;
 	createBackground("button-panel");
 
-	if (parent->info->dismissInfo->callback)
+	if (parent->info->dismissInfo && parent->info->dismissInfo->callback)
 	{
 		auto onDismiss = [=]()
 		{