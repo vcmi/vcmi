78d02cb1eb7810b4f4bffe51545569e1c3cd9764
Ivan Savenko <saven.ivan@gmail.com>
2015-05-02 09:01:48 +0300

    Linux version bump

--- lib/GameConstants.h
+++ lib/GameConstants.h
@@ -14,7 +14,7 @@
 
 namespace GameConstants
 {
-	const std::string VCMI_VERSION = "VCMI 0.98";
+	const std::string VCMI_VERSION = "VCMI 0.98b";
 
 	const int BFIELD_WIDTH = 17;
 	const int BFIELD_HEIGHT = 11;
