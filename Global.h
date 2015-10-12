337276604d896a5e30ef0fa67442df3d2db4cfd4
Vadim Markovtsev <v.markovtsev@samsung.com>
2015-10-12 08:33:52 +0300

    Fix detection of VCMI_APPLE

--- Global.h
+++ Global.h
@@ -61,7 +61,7 @@ static_assert(sizeof(bool) == 1, "Bool needs to be 1 byte in size.");
 #  define VCMI_UNIX
 #  define VCMI_XDG
 #  define VCMI_FREEBSD
-#elif defined(__GNU__) || defined(__gnu_hurd__) || (defined(__MACH__) && !defined(__APPLE))
+#elif defined(__GNU__) || defined(__gnu_hurd__) || (defined(__MACH__) && !defined(__APPLE__))
 #  define VCMI_UNIX
 #  define VCMI_XDG
 #  define VCMI_HURD
