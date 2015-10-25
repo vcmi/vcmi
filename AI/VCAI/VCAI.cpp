2a59cb619137cb443adce6aba009ecf37a6a57da
DjWarmonger <Warmonger@vp.pl>
2015-10-25 11:55:50 +0100

    Fixed #2308 - AI didn't attack enemy heroes at all.

--- AI/VCAI/VCAI.cpp
+++ AI/VCAI/VCAI.cpp
@@ -3351,7 +3351,7 @@ int3 SectorMap::findFirstVisitableTile (HeroPtr h, crint3 dst)
 	while(curtile != h->visitablePos())
 	{
 		auto topObj = cb->getTopObj(curtile);
-		if (topObj && topObj->ID == Obj::HERO && topObj != h.h)
+		if (topObj && topObj->ID == Obj::HERO && h->tempOwner == topObj->tempOwner && topObj != h.h)
 		{
 			logAi->warnStream() << ("Another allied hero stands in our way");
 			return ret;
