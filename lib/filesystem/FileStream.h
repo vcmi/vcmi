2a55d5f85ea873e97a650054bfb2f4e9fcedca26
Zyx-2000 <kingsbot1@gmail.com>
2016-01-19 14:26:18 +0100

    added copyright info

--- lib/filesystem/FileStream.h
+++ lib/filesystem/FileStream.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/*
+ * FileStream.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
 #include <boost/iostreams/categories.hpp>
 #include <boost/iostreams/stream.hpp>
 
