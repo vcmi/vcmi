7185890723031c033b87468a9f4edf4ae34c9b2c
Vadim Markovtsev <v.markovtsev@samsung.com>
2016-01-24 01:31:39 +0300

    Fix double free in battleAfterLevelUp() in case of a draw

--- server/CGameHandler.cpp
+++ server/CGameHandler.cpp
@@ -741,7 +741,7 @@ void CGameHandler::battleAfterLevelUp( const BattleResult &result )
 
 		sendAndApply(&sah);
 	}
-	if(finishingBattle->winnerHero && finishingBattle->winnerHero->stacks.empty())
+	if(result.winner != 2 && finishingBattle->winnerHero && finishingBattle->winnerHero->stacks.empty())
 	{
 		RemoveObject ro(finishingBattle->winnerHero->id);
 		sendAndApply(&ro);
