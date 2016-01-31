b0bb7f1f254f1330975c8ba4331846bba6f6624b
Zyx-2000 <kingsbot1@gmail.com>
2016-01-31 11:13:56 +0100

    copyright stuff

--- client/SDLRWwrapper.h
+++ client/SDLRWwrapper.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/*
+ * SDLRWwrapper.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
 struct SDL_RWops;
 class CInputStream;
 
