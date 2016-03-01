fe4d871fe7afcc3ee56c67dcd044481a3ede40bc
DjWarmonger <Warmonger@vp.pl>
2016-03-01 19:42:40 +0100

    Version bump.

--- lib/GameConstants.h
+++ lib/GameConstants.h
@@ -14,7 +14,7 @@
 
 namespace GameConstants
 {
-	const std::string VCMI_VERSION = "VCMI 0.98g";
+	const std::string VCMI_VERSION = "VCMI 0.98h";
 
 	const int BFIELD_WIDTH = 17;
 	const int BFIELD_HEIGHT = 11;
