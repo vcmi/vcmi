d968997a119f40480671f09502f3936c4cc9868e
Argarak <kzer-za@cryptolab.net>
2016-03-07 22:46:41 +0000

    Fixed crash when right-clicking on artifacts in the Black Market.

---
+++ build/test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kzer-za/vcmi/test
+# Build directory: /home/kzer-za/vcmi/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(vcmitest "vcmitest")
