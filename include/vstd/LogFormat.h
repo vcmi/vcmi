72f79a3ad7ddd49f72ce8c6d17c22a709eebeaf6
AlexVinS <alexvins@mail.ru>
2016-08-12 09:38:26 +0300

    Draft boost::format based log proxy.

---
+++ include/vstd/LogFormat.h
@@ -0,0 +1,20 @@
+/*
+ * CLogFormat.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
+#pragma once
+
+namespace vstd
+{
+    template<typename Logger, typename ... Args>
+    void logDebugFormat(Logger * logger, const std::string & format, Args ... args)
+    {
+
+    }
+}
--- lib/StdInc.h
+++ lib/StdInc.h
@@ -2,6 +2,9 @@
 
 #include "../Global.h"
 
+//todo: move to Global.h
+#include <vstd/LogFormat.h>
+
 // This header should be treated as a pre compiled header file(PCH) in the compiler building settings.
 
 // Here you can add specific libraries and macros which are specific to this project.
