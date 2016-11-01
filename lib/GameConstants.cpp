db13d7a708b9b862f16269ff4d2995b3a564d8fe
DjWarmonger <Warmonger@vp.pl>
2016-11-01 18:25:09 +0100

    Version 0.99 updates.

--- lib/GameConstants.cpp
+++ lib/GameConstants.cpp
@@ -38,9 +38,9 @@ const TeamID TeamID::NO_TEAM = TeamID(255);
 namespace GameConstants
 {
 #ifdef VCMI_NO_EXTRA_VERSION
-	const std::string VCMI_VERSION = std::string("VCMI 0.98h");
+	const std::string VCMI_VERSION = std::string("VCMI 0.99");
 #else
-	const std::string VCMI_VERSION = std::string("VCMI 0.98h ") + GIT_SHA1;
+	const std::string VCMI_VERSION = std::string("VCMI 0.99 ") + GIT_SHA1;
 #endif
 }
 
