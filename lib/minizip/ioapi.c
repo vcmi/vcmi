eda37feb0f2a362da74d66e2361546282d539c19
Fay <no.email@at.all>
2017-05-21 10:20:59 +0200

    Removed android ioapi hack (fixed in newest ndk);

--- lib/minizip/ioapi.c
+++ lib/minizip/ioapi.c
@@ -14,11 +14,7 @@
         #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#if defined(IOAPI64_ANDROID_HACK) // https://github.com/android-ndk/ndk/issues/364 
-#define FOPEN_FUNC(filename, mode) fopen(filename, mode)
-#define FTELLO_FUNC(stream) ftell(stream)
-#define FSEEKO_FUNC(stream, offset, origin) fseek(stream, offset, origin)
-#elif defined(__APPLE__) || defined(IOAPI_NO_64)
+#if defined(__APPLE__) || defined(IOAPI_NO_64)
 // In darwin and perhaps other BSD variants off_t is a 64 bit value, hence no need for specific 64 bit functions
 #define FOPEN_FUNC(filename, mode) fopen(filename, mode)
 #define FTELLO_FUNC(stream) ftello(stream)
