34058f1396d1d3468bfaeadbb642e1b76409efaf
Alex Dunn <dunn.alex@gmail.com>
2017-07-05 23:09:30 -0700

    macOS: turn Sparkle off by default (#327)

--- client/Autoupdate.mm
+++ client/Autoupdate.mm
@@ -1,7 +1,11 @@
+#ifdef SPARKLE
 #import <Cocoa/Cocoa.h>
 #import "Sparkle.h"
+#endif
 
 void OSX_checkForUpdates() {
+#ifdef SPARKLE
     SUUpdater* updater = [[SUUpdater alloc] init];
     [[SUUpdater sharedUpdater] checkForUpdatesInBackground];
+#endif
 }
