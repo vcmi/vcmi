ad20528a7b9631784132c26b2472feb8b8f82ebe
godric3 <zakandrzej3@gmail.com>
2017-12-27 11:12:09 +0100

    fix osx build

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 brew update
-brew unlink boost
-brew install boost smpeg2 innoextract libpng freetype sdl2 sdl2_ttf sdl2_image qt5 ffmpeg
+brew install smpeg2 libpng freetype sdl2 sdl2_ttf sdl2_image qt5 ffmpeg
 brew install sdl2_mixer --with-smpeg2
 
 export CMAKE_PREFIX_PATH="/usr/local/opt/qt5:$CMAKE_PREFIX_PATH"
