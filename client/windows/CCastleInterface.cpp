d2439ffc33b99db7c0f5be7312b864dc2f433045
Aleksei Iupinov <a.yupinov@gmail.com>
2018-08-04 14:35:31 +0200

    Correct the mage guild check order

--- client/windows/CCastleInterface.cpp
+++ client/windows/CCastleInterface.cpp
@@ -877,17 +877,18 @@ void CCastleBuildings::enterMagesGuild()
 	if(hero && !hero->hasSpellbook()) //hero doesn't have spellbok
 	{
 		const StartInfo *si = LOCPLINT->cb->getStartInfo();
-		if(LOCPLINT->cb->getResourceAmount(Res::GOLD) < 500) //not enough gold to buy spellbook
-		{
-			openMagesGuild();
-			LOCPLINT->showInfoDialog(CGI->generaltexth->allTexts[213]);
-		}
-		else if(si && si->campState && si->campState->camp &&           // We're in campaign,
+		// it would be nice to find a way to move this hack to config/mapOverrides.json
+		if (si && si->campState && si->campState->camp &&               // We're in campaign,
 			(si->campState->camp->header.filename == "DATA/YOG.H3C") && // which is "Birth of a Barbarian",
 			(hero->subID == 45) && (hero->name == "Yog"))               // and the hero is Yog (based on Solmyr)
 		{
 			// "Yog has given up magic in all its forms..."
 			LOCPLINT->showInfoDialog(CGI->generaltexth->allTexts[736]);
+		}
+		else if(LOCPLINT->cb->getResourceAmount(Res::GOLD) < 500) //not enough gold to buy spellbook
+		{
+			openMagesGuild();
+			LOCPLINT->showInfoDialog(CGI->generaltexth->allTexts[213]);
 		}
 		else
 		{
