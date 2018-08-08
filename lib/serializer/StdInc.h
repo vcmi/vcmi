036b74db8e1976240e2ca83e40f66634546dd6df
Konstantin Vukolov <kasyanvukolov@yandex.ru>
2018-08-08 16:04:22 +0300

    Remove unused includes

--- lib/serializer/StdInc.h
+++ lib/serializer/StdInc.h
@@ -9,15 +9,9 @@
  */
 #pragma once
 
-#include "../Global.h"
-#include "../lib/serializer/CTypeList.h"
-
-#include <boost/crc.hpp>
-#include <boost/date_time/posix_time/posix_time_types.hpp> //no i/o just types
-#include <boost/random/linear_congruential.hpp>
-#include <boost/random/mersenne_twister.hpp>
-#include <boost/random/variate_generator.hpp>
-#include <boost/system/system_error.hpp>
+#include <typeinfo>
+#include <string>
+#include "CTypeList.h"
 
 template<class T, class F>
 inline const T * dynamic_ptr_cast(const F * ptr)
