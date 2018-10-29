c8ba3de8213fdf544add581e16181e7b48461b6b
godric3 <zakandrzej3@gmail.com>
2018-10-29 20:53:13 +0100

    Change skill bonuses description to use name instead of identifier

--- lib/CSkillHandler.cpp
+++ lib/CSkillHandler.cpp
@@ -46,7 +46,7 @@ void CSkill::addNewBonus(const std::shared_ptr<Bonus> & b, int level)
 	b->source = Bonus::SECONDARY_SKILL;
 	b->sid = id;
 	b->duration = Bonus::PERMANENT;
-	b->description = identifier;
+	b->description = name;
 	levels[level-1].effects.push_back(b);
 }
 
