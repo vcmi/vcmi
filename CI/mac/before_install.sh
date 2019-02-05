0ccdd315860bbc205d5bc82220cf1307b9727a87
AlexVinS <alexvins@mail.ru>
2019-02-05 08:34:37 +0300

    Fixed broken OSX build.

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -2,6 +2,6 @@
 
 brew update
 brew install smpeg2 libpng freetype sdl2 sdl2_ttf sdl2_image qt5 ffmpeg ninja
-brew install sdl2_mixer --with-smpeg2
+brew install sdl2_mixer
 
 export CMAKE_PREFIX_PATH="/usr/local/opt/qt5:$CMAKE_PREFIX_PATH"
