0ce7c36535b69b42825c6ab76bee77eac1baa277
Andrii Danylchenko <danilchenko.andrij@gmail.com>
2019-04-07 07:31:47 +0300

    ai fixes for android

--- AI/VCAI/Goals/CompleteQuest.cpp
+++ AI/VCAI/Goals/CompleteQuest.cpp
@@ -12,8 +12,8 @@
 #include "../VCAI.h"
 #include "../FuzzyHelper.h"
 #include "../AIhelper.h"
-#include "../../lib/mapping/CMap.h" //for victory conditions
-#include "../../lib/CPathfinder.h"
+#include "../../../lib/mapping/CMap.h" //for victory conditions
+#include "../../../lib/CPathfinder.h"
 
 extern boost::thread_specific_ptr<CCallback> cb;
 extern boost::thread_specific_ptr<VCAI> ai;
