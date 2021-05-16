1a69a43f095f286295d41ced59b244f04ac20d76
Andrii Danylchenko <danilchenko.andrij@gmail.com>
2021-05-16 14:45:17 +0300

    Nullkiller: fix border gate and garrison bypassing when owned

--- AI/Nullkiller/Pathfinding/Rules/AIPreviousNodeRule.cpp
+++ AI/Nullkiller/Pathfinding/Rules/AIPreviousNodeRule.cpp
@@ -23,8 +23,23 @@ namespace AIPathfinding
 		const PathfinderConfig * pathfinderConfig,
 		CPathfinderHelper * pathfinderHelper) const
 	{
-		if(source.node->action == CGPathNode::ENodeAction::BLOCKING_VISIT || source.node->action == CGPathNode::ENodeAction::VISIT)
+		if(source.node->action == CGPathNode::ENodeAction::BLOCKING_VISIT 
+			|| source.node->action == CGPathNode::ENodeAction::VISIT)
 		{
+			if(source.nodeObject)
+			{
+				if((source.nodeObject->ID == Obj::GARRISON || source.nodeObject->ID == Obj::GARRISON2)
+					&& source.heroRelations != PlayerRelations::ENEMIES)
+					return;
+
+				if(source.nodeObject->ID == Obj::BORDER_GATE)
+				{
+					auto quest = dynamic_cast<const CGBorderGate *>(source.nodeObject);
+
+					if(quest->wasMyColorVisited(pathfinderHelper->hero->tempOwner))
+						return;
+				}
+			}
 			// we can not directly bypass objects, we need to interact with them first
 			destination.node->theNodeBefore = source.node;
 
