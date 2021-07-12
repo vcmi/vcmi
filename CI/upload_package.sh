7252553857fc4ac383b4a2bd88dd64d330df7b72
Andrii Danylchenko <danilchenko.andrij@gmail.com>
2021-07-12 22:00:29 +0300

    Disable publishing builds to download.vcmi.eu except develop. Added artifacts

--- CI/upload_package.sh
+++ CI/upload_package.sh
@@ -6,12 +6,10 @@ then
 	exit 0
 fi
 
-cpack
-
 echo "$DEPLOY_RSA" > /tmp/deploy_rsa
 chmod 600 /tmp/deploy_rsa
 
 eval "$(ssh-agent -s)"
 ssh-add /tmp/deploy_rsa
 
-sftp -r -o StrictHostKeyChecking=no travis@beholder.vcmi.eu <<< "put $VCMI_PACKAGE_FILE_NAME.exe /incoming/$VCMI_PACKAGE_FILE_NAME.exe"
+sftp -r -o StrictHostKeyChecking=no travis@beholder.vcmi.eu <<< "put $VCMI_PACKAGE_FILE_NAME.$PACKAGE_EXTENSION /incoming/$VCMI_PACKAGE_FILE_NAME.$PACKAGE_EXTENSION"
