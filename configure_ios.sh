fb32dd0ffcb4fadead4858428b066f3e0087bc43
Andrey Filipenkov <decapitator@ukr.net>
2021-11-28 20:46:40 +0300

    adapt config to new dependencies

--- configure_ios.sh
+++ configure_ios.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-boostPrefix=~/dev/other/Apple-Boost-BuildScript/build-static/boost/1.75.0/ios/debug/prefix
-ffmpegDir=~/dev/ios/vcmi-ios-deps/mobile-ffmpeg-min-universal-4.4
-sdlLibsDir=~/dev/ios/vcmi-ios-deps/SDL2-lib
+platform=OS64
+globalPrefix=~/dev/vcmi/vcmi-ios-depends/build/iphoneos
 qtDir=~/dev/Qt-libs/5.15.2/ios
-tbbDir=~/dev/ios/vcmi-ios-deps/oneTBB-2021.4.0/build/install
-luajitDir=~/dev/ios/vcmi-ios-deps/LuaJIT-2.1/ios64
 if [[ "$1" ]]; then
-	tbbDir=~/dev/ios/vcmi-ios-deps/oneTBB-2021.4.0/build-sim/install
-	luajitDir=~/dev/ios/vcmi-ios-deps/LuaJIT-2.1/sim64
+	platform=SIMULATOR64
+	globalPrefix=~/dev/vcmi/vcmi-ios-depends/build/iphonesimulator
 fi
-prefixPath="$boostPrefix;$ffmpegDir;$sdlLibsDir;$qtDir;$tbbDir;$luajitDir"
+prefixPath="$globalPrefix;$qtDir"
 
 # prefixPath="$boostPrefix;$sdlLibsDir"
 # xcodeMajorVersion=$(xcodebuild -version | fgrep Xcode | cut -d ' ' -f 2 | cut -d . -f 1)
@@ -28,15 +25,11 @@ cmake "$srcDir" -G Xcode \
   -Wno-dev \
   -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
   -DCMAKE_TOOLCHAIN_FILE="$srcDir/ios.toolchain.cmake" \
-  -DPLATFORM=${1:-OS64} \
+  -DPLATFORM=$platform \
   -DDEPLOYMENT_TARGET=11.0 \
   -DENABLE_BITCODE=0 \
   -DCMAKE_BINARY_DIR=$(pwd) \
   -DCMAKE_PREFIX_PATH="$prefixPath" \
-  -DSDL2_INCLUDE_DIR=~/dev/ios/vcmi-ios-deps/SDL/include \
-  -DSDL2_IMAGE_INCLUDE_DIR=~/dev/ios/vcmi-ios-deps/SDL_image-release-2.0.5 \
-  -DSDL2_MIXER_INCLUDE_DIR=~/dev/ios/vcmi-ios-deps/SDL_mixer-release-2.0.4 \
-  -DSDL2_TTF_INCLUDE_DIR=~/dev/ios/vcmi-ios-deps/SDL_ttf-release-2.0.15 \
   -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY='Apple Development' \
   -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM='4XHN44TEVG'
   # -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGNING_ALLOWED=NO
