8319c306957dde7373c6ba1bde72fa0554302372
Andrey Filipenkov <decapitator@ukr.net>
2021-12-02 13:53:54 +0300

    copy external TBB dylib to app package

--- apple_codesign.sh
+++ apple_codesign.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ "$PLATFORM_NAME" != "iphoneos" ]]; then
+if [[ "$PLATFORM_NAME" == *simulator* || -z "$EXPANDED_CODE_SIGN_IDENTITY" ]]; then
   exit 0
 fi
 
 echo 'codesign dylibs'
-for lib in $(find "$CODESIGNING_FOLDER_PATH/Frameworks" -iname '*.dylib'); do
-  codesign --force --timestamp=none --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$lib"
+for lib in $(find "$CODESIGNING_FOLDER_PATH/Frameworks" -type f -iname '*.dylib'); do
+  codesign --verbose=4 --force --timestamp=none --sign "$EXPANDED_CODE_SIGN_IDENTITY" "$lib"
 done
