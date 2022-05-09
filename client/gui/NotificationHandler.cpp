f9afaad5a11fbc244ddb2390b5373d3c894263e9
Andrii Danylchenko <danilchenko.andrij@gmail.com>
2022-05-09 11:54:45 +0300

    Tray notifications for MXE

--- client/gui/NotificationHandler.cpp
+++ client/gui/NotificationHandler.cpp
@@ -13,7 +13,7 @@
 #include <SDL_video.h>
 #include <SDL_syswm.h>
 
-#if defined(VCMI_WINDOWS) && defined(_WIN64)
+#if defined(VCMI_WINDOWS)
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
