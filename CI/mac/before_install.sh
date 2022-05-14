45bdacb11975740293013dd27ba6aa0d0dcf3997
Konstantin <ria.freelander@gmail.com>
2022-05-14 12:38:07 +0300

    mac: use stable ffmpeg for CI

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -2,7 +2,7 @@
 
 brew update
 #brew pin python@3.9
-brew install smpeg2 libpng freetype qt5 ffmpeg@4 ninja boost tbb luajit
+brew install smpeg2 libpng freetype qt5 ffmpeg ninja boost tbb luajit
 brew install sdl2 sdl2_ttf sdl2_image sdl2_mixer
 
 echo CMAKE_PREFIX_PATH="/usr/local/opt/ffmpeg@4:/usr/local/opt/qt5:$CMAKE_PREFIX_PATH" >> $GITHUB_ENV
