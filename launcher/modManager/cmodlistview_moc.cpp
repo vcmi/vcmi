41c4d0f12590d8be149158e23f978c2e2b4a9e33
Konstantin <ria.freelander@gmail.com>
2022-05-27 11:23:40 +0300

    launcher: added regex ifdef

--- launcher/modManager/cmodlistview_moc.cpp
+++ launcher/modManager/cmodlistview_moc.cpp
@@ -354,10 +354,14 @@ void CModListView::on_allModsView_activated(const QModelIndex & index)
 void CModListView::on_lineEdit_textChanged(const QString & arg1)
 {
 	auto reOptions =  QRegularExpression::CaseInsensitiveOption;
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+	auto baseStr = QRegularExpression::wildcardToRegularExpression(arg1, QRegularExpression::UnanchoredWildcardConversion);
+#else
 	auto baseStr = QRegularExpression::wildcardToRegularExpression(arg1);
 	//Hack due to lack QRegularExpression::UnanchoredWildcardConversion in Qt5
 	baseStr.chop(3);
 	baseStr.remove(0,5);
+#endif
 	QRegularExpression regExp{baseStr, reOptions};
 	filterModel->setFilterRegularExpression(regExp);
 }
