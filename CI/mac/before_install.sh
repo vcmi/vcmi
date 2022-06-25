833f13056267048afd0225383827a374d0931adc
Max Desiatov <max@desiatov.com>
2022-06-25 14:37:47 +0100

    Support Homebrew on M1
    Homebrew uses prefix path on M1 (`/opt/homebrew`) different from x86_64 Homebrew path. It can be queried with `brew --prefix` command, which allows supporting both platforms in `mac/before_install.sh`.

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -5,4 +5,4 @@ brew update
 brew install smpeg2 libpng freetype qt5 ffmpeg ninja boost tbb luajit
 brew install sdl2 sdl2_ttf sdl2_image sdl2_mixer
 
-echo CMAKE_PREFIX_PATH="/usr/local/opt/ffmpeg@4:/usr/local/opt/qt5:$CMAKE_PREFIX_PATH" >> $GITHUB_ENV
+echo CMAKE_PREFIX_PATH="$(brew --prefix)/opt/ffmpeg@4:$(brew --prefix)/opt/qt5:$CMAKE_PREFIX_PATH" >> $GITHUB_ENV
