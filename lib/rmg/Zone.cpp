bb96a450194787dad06e515779a1cf79e2a90d7b
nordsoft <nordsoft@rambler.ru>
2022-07-18 00:12:05 +0300

    Revert "Change pathfinding algorithm to avoid borders"
    This reverts commit 2e0e979c48c6f381c1824c20e8d291d95200c044.

--- lib/rmg/Zone.cpp
+++ lib/rmg/Zone.cpp
@@ -144,15 +144,13 @@ void Zone::setTerrainType(const Terrain & terrain)
 rmg::Path Zone::searchPath(const rmg::Area & src, bool onlyStraight, std::function<bool(const int3 &)> areafilter) const
 ///connect current tile to any other free tile within zone
 {
-	rmg::Area border(dArea.getBorder());
-	auto movementCost = [this, &border](const int3 & s, const int3 & d)
+	auto movementCost = [this](const int3 & s, const int3 & d)
 	{
-		float cost = 1.f / (1.f + border.distanceSqr(d));
 		if(map.isFree(d))
-			return 1.f + cost;
+			return 1;
 		else if (map.isPossible(d))
-			return 2.f + cost;
-		return 3.f + cost;
+			return 2;
+		return 3;
 	};
 	
 	auto area = (dAreaPossible + dAreaFree).getSubarea(areafilter);
