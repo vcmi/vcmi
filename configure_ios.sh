1bb6a20b769ceca2c57f63ee1e8e680f2bab1d82
Andrey Filipenkov <decapitator@ukr.net>
2022-08-08 17:38:57 +0300

    get rid of the custom toolchain

--- configure_ios.sh
+++ configure_ios.sh
@@ -1,34 +1,18 @@
 #!/usr/bin/env bash
 
-platform=OS64
-globalPrefix=~/dev/vcmi/vcmi-ios-depends/build/iphoneos
-qtDir=~/dev/Qt-libs/5.15.5/ios10-widgets
+prefixPath=~/dev/vcmi/vcmi-ios-depends/build/iphoneos
 if [[ "$1" ]]; then
-	platform=SIMULATOR64
-	globalPrefix=~/dev/vcmi/vcmi-ios-depends/build/iphonesimulator
+	prefixPath=~/dev/vcmi/vcmi-ios-depends/build/iphonesimulator
 fi
-prefixPath="$globalPrefix;$qtDir"
 
-# prefixPath="$boostPrefix;$sdlLibsDir"
-# xcodeMajorVersion=$(xcodebuild -version | fgrep Xcode | cut -d ' ' -f 2 | cut -d . -f 1)
-# if [[ $xcodeMajorVersion -ge 12 ]]; then
-#   extraVars=-DCMAKE_FRAMEWORK_PATH=~/dev/ios/vcmi-ios-deps/mobile-ffmpeg-min-gpl-4.4-xc12-frameworks
-# else
-#   prefixPath+=;~/dev/ios/vcmi-ios-deps/mobile-ffmpeg-min-universal
-# fi
+# -DCMAKE_OSX_SYSROOT=iphonesimulator
 
-srcDir="../vcmi"
-# cmake "$srcDir" -G Xcode -T buildsystem=1 \
-cmake "$srcDir" -G Xcode \
+cmake ../vcmi -G Xcode \
+  -DCMAKE_SYSTEM_NAME=iOS \
   -DFORCE_BUNDLED_MINIZIP=ON \
   -DBUNDLE_IDENTIFIER_PREFIX=com.kambala \
   -Wno-dev \
   -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
-  --toolchain "$srcDir/ios.toolchain.cmake" \
-  -DPLATFORM=$platform \
-  -DDEPLOYMENT_TARGET=12.0 \
-  -DENABLE_BITCODE=OFF \
-  -DCMAKE_BINARY_DIR=$(pwd) \
   -DCMAKE_PREFIX_PATH="$prefixPath" \
   -DCMAKE_XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY='Apple Development' \
   -DCMAKE_XCODE_ATTRIBUTE_DEVELOPMENT_TEAM='4XHN44TEVG'
