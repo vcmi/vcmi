b8b96e7548bf5eb5c1027faa51007c392d455302
Andrey Filipenkov <decapitator@ukr.net>
2022-08-25 10:22:23 +0300

    [CI] use Conan to create macOS-ARM builds targeting 11.0

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -5,5 +5,5 @@ echo DEVELOPER_DIR=/Applications/Xcode_13.4.1.app >> $GITHUB_ENV
 brew install ninja
 
 mkdir ~/.conan ; cd ~/.conan
-curl -L "https://github.com/vcmi/vcmi-deps-macos/releases/latest/download/intel.txz" \
+curl -L "https://github.com/vcmi/vcmi-deps-macos/releases/latest/download/$DEPS_FILENAME.txz" \
 	| tar -xf -
