c701ee61cc1fb66a980387b9e3fa88868a7ca832
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2022-09-03 18:26:11 +0200

    Create dirt roads to mines if there are no roads in a zone.
    Signed-off-by: Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>

--- lib/rmg/CMapGenerator.h
+++ lib/rmg/CMapGenerator.h
@@ -38,6 +38,7 @@ public:
 		int mineExtraResources;
 		int minGuardStrength;
 		std::string defaultRoadType;
+		std::string secondaryRoadType;
 		int treasureValueLimit;
 		std::vector<int> prisonExperience, prisonValues;
 		std::vector<int> scrollValues;
