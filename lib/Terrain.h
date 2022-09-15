98edd6801bffee774944bbf9e7483e112881dcd4
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2022-09-15 12:09:11 +0200

    Cleaning up some unneccessary changes

--- lib/Terrain.h
+++ lib/Terrain.h
@@ -1,12 +1,12 @@
 /*
-* Terrain.h, part of VCMI engine
-*
-* Authors: listed in file AUTHORS in main folder
-*
-* License: GNU General Public License v2.0 or later
-* Full text of license available in license.txt file, in main folder
-*
-*/
+ * Terrain.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
 
 #pragma once
 
