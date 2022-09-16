7b4412758ce9c05c664b244174b729fcbf0fc76c
Andrey Filipenkov <decapitator@ukr.net>
2022-09-16 19:53:45 +0300

    [CI] download dependencies from VCMI repo

--- CI/ios/before_install.sh
+++ CI/ios/before_install.sh
@@ -2,6 +2,6 @@
 
 echo DEVELOPER_DIR=/Applications/Xcode_13.4.1.app >> $GITHUB_ENV
 
-curl -L 'https://github.com/kambala-decapitator/vcmi-ios-depends/releases/latest/download/vcmi-ios-depends-xc13.2.1.txz' \
+curl -L 'https://github.com/vcmi/vcmi-ios-deps/releases/latest/download/vcmi-ios-depends-xc13.2.1.txz' \
 	| tar -xf -
 build/fix_install_paths.command
