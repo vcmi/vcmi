7801a2ad85c3c3219614863a4bb5c6f35a294997
Andrey Filipenkov <decapitator@ukr.net>
2022-09-30 12:52:45 +0300

    [iOS] show app and bundle version in system settings

--- ios/set_build_version.sh
+++ ios/set_build_version.sh
@@ -5,3 +5,6 @@ commitShort=$(git rev-parse --short HEAD)
 bundleVersion="$today-$commitShort"
 
 /usr/libexec/PlistBuddy "$1/Info.plist" -c "Set :CFBundleVersion $bundleVersion"
+
+/usr/libexec/PlistBuddy "$1/Settings.bundle/Root.plist" -c "Set :PreferenceSpecifiers:0:DefaultValue $MARKETING_VERSION"
+/usr/libexec/PlistBuddy "$1/Settings.bundle/Root.plist" -c "Set :PreferenceSpecifiers:1:DefaultValue $bundleVersion"
