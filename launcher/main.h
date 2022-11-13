b2b9cacac2d7e9783dea4fd08b37b87c6e192d66
nordsoft <nordsoft@rambler.ru>
2022-11-13 05:22:19 +0400

    Fix mxe build

--- launcher/main.h
+++ launcher/main.h
@@ -8,12 +8,12 @@
  *
  */
 #pragma once
-extern int __argc;
-extern char ** __argv;
 
 void startGame(const QStringList & args);
 
 #ifdef VCMI_IOS
+extern int argcForClient;
+extern char** argvForClient;
 extern "C" void launchGame(int argc, char * argv[]);
 #else
 void startExecutable(QString name, const QStringList & args);
