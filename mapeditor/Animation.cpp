7bac01f10ac66ed752e6a6144b8264df65af16e0
nordsoft <nordsoft@rambler.ru>
2022-11-19 00:01:48 +0400

    More fixes on image loader

--- mapeditor/Animation.cpp
+++ mapeditor/Animation.cpp
@@ -450,10 +450,10 @@ void ImageLoader::init(QPoint SpriteSize, QPoint Margins, QPoint FullSize)
 	margins = Margins;
 	fullSize = FullSize;
 	
-	memset((void *)image->bits(), 0, fullSize.y() * fullSize.x());
+	memset((void *)image->bits(), 0, fullSize.y() * image->bytesPerLine());
 	
 	lineStart = image->bits();
-	lineStart += margins.y() * fullSize.x() + margins.x();
+	lineStart += margins.y() * image->bytesPerLine + margins.x();
 	position = lineStart;
 }
 
