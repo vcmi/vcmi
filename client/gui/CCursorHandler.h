ba200fb079df9cce6bd92f6ab773917b7c6eff17
Ivan Savenko <saven.ivan@gmail.com>
2022-12-18 17:13:34 +0200

    Added enum's for rest of cursor types

--- client/gui/CCursorHandler.h
+++ client/gui/CCursorHandler.h
@@ -18,8 +18,14 @@ namespace ECursor
 	enum ECursorTypes {
 		ADVENTURE, // set of various cursors for adventure map
 		COMBAT,    // set of various cursors for combat
-		DEFAULT,   // default arror and hourglass cursors
+		DEFAULT,   // default arrow and hourglass cursors
 		SPELLBOOK  // animated cursor for spellcasting
+	};
+
+	enum EDefaultCursors {
+		DEFAULT_ARROW      = 0,
+		DEFAULT_ARROW_COPY = 1, // probably unused
+		DEFAULT_HOURGLASS  = 2,
 	};
 
 	enum EBattleCursors {
@@ -43,6 +49,52 @@ namespace ECursor
 		COMBAT_HEAL           = 17,
 		COMBAT_SACRIFICE      = 18,
 		COMBAT_TELEPORT       = 19
+	};
+
+	enum EAdventureCursors {
+		ADV_ARROW            =  0,
+		ADV_HOURGLASS        =  1,
+		ADV_HERO             =  2,
+		ADV_TOWN             =  3,
+		ADV_T1_MOVE          =  4,
+		ADV_T1_ATTACK        =  5,
+		ADV_T1_SAIL          =  6,
+		ADV_T1_DISEMBARK     =  7,
+		ADV_T1_EXCHANGE      =  8,
+		ADV_T1_VISIT         =  9,
+		ADV_T2_MOVE          = 10,
+		ADV_T2_ATTACK        = 11,
+		ADV_T2_SAIL          = 12,
+		ADV_T2_DISEMBARK     = 13,
+		ADV_T2_EXCHANGE      = 14,
+		ADV_T2_VISIT         = 15,
+		ADV_T3_MOVE          = 16,
+		ADV_T3_ATTACK        = 17,
+		ADV_T3_SAIL          = 18,
+		ADV_T3_DISEMBARK     = 19,
+		ADV_T3_EXCHANGE      = 20,
+		ADV_T3_VISIT         = 21,
+		ADV_T4_MOVE          = 22,
+		ADV_T4_ATTACK        = 23,
+		ADV_T4_SAIL          = 24,
+		ADV_T4_DISEMBARK     = 25,
+		ADV_T4_EXCHANGE      = 26,
+		ADV_T4_VISIT         = 27,
+		ADV_T1_SAIL_VISIT    = 28,
+		ADV_T2_SAIL_VISIT    = 29,
+		ADV_T3_SAIL_VISIT    = 30,
+		ADV_T4_SAIL_VISIT    = 31,
+		ADV_SCROLL_NORTH     = 32,
+		ADV_SCROLL_NORTHEAST = 33,
+		ADV_SCROLL_EAST      = 34,
+		ADV_SCROLL_SOUTHEAST = 35,
+		ADV_SCROLL_SOUTH     = 36,
+		ADV_SCROLL_SOUTHWEST = 37,
+		ADV_SCROLL_WEST      = 38,
+		ADV_SCROLL_NORTHWEST = 39,
+		ADV_ARROW_COPY       = 40, // probably unused
+		ADV_TELEPORT         = 41,
+		ADV_SCUTTLE_SHIP     = 42
 	};
 }
 
