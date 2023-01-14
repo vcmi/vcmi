aabc5afeef46e77c73d4988cc2d9ecefd32f93e9
nordsoft <nordsoft@rambler.ru>
2023-01-15 01:49:12 +0400

    Try to fix mac build

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -2,8 +2,10 @@
 
 echo DEVELOPER_DIR=/Applications/Xcode_13.4.1.app >> $GITHUB_ENV
 
-brew install ninja enet
+brew install ninja
 
 mkdir ~/.conan ; cd ~/.conan
 curl -L "https://github.com/vcmi/vcmi-deps-macos/releases/download/1.1/$DEPS_FILENAME.txz" \
 	| tar -xf -
+
+brew install enet
