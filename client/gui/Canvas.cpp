4fb554847529c3b7a3b4054863be657a8d1b007e
Dydzio <blood990@gmail.com>
2023-01-15 00:09:28 +0100

    Remove unused stuff from code

--- client/gui/Canvas.cpp
+++ client/gui/Canvas.cpp
@@ -70,13 +70,6 @@ void Canvas::draw(std::shared_ptr<IImage> image, const Point & pos, const Rect &
 		image->draw(surface, renderOffset.x + pos.x, renderOffset.y + pos.y, &sourceRect);
 }
 
-//void Canvas::draw(std::shared_ptr<IImage> image, const Point & pos, const Rect & sourceRect, uint8_t alpha)
-//{
-//	assert(image);
-//	if (image)
-//		image->draw(surface, renderOffset.x + pos.x, renderOffset.y + pos.y, &sourceRect, alpha);
-//}
-
 void Canvas::draw(Canvas & image, const Point & pos)
 {
 	blitAt(image.surface, renderOffset.x + pos.x, renderOffset.y + pos.y, surface);
