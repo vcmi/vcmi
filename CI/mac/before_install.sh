8b39119a64df8cf8ca082e1e29aa0084693132b3
nordsoft <nordsoft@rambler.ru>
2023-01-15 01:14:06 +0400

    Add deps for linux and mac

--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -2,7 +2,7 @@
 
 echo DEVELOPER_DIR=/Applications/Xcode_13.4.1.app >> $GITHUB_ENV
 
-brew install ninja
+brew install ninja enet
 
 mkdir ~/.conan ; cd ~/.conan
 curl -L "https://github.com/vcmi/vcmi-deps-macos/releases/download/1.1/$DEPS_FILENAME.txz" \
