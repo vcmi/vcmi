4deb36742d81cc8b39ec233c8b578133a4340016
Ivan Savenko <saven.ivan@gmail.com>
2023-01-15 14:05:01 +0200

    Updated MSVC prebuilt package

--- CI/msvc/before_install.sh
+++ CI/msvc/before_install.sh
@@ -1,6 +1,6 @@
 curl -LfsS -o "vcpkg-export-${VCMI_BUILD_PLATFORM}-windows-v140.7z" \
-	"https://github.com/vcmi/vcmi-deps-windows/releases/download/v1.5/vcpkg-export-${VCMI_BUILD_PLATFORM}-windows-v140.7z"
-7z x "vcpkg-export-${VCMI_BUILD_PLATFORM}-windows-v140.7z"
+	"https://github.com/vcmi/vcmi-deps-windows/releases/download/v1.6/vcpkg-export-${VCMI_BUILD_PLATFORM}-windows-v143.7z"
+7z x "vcpkg-export-${VCMI_BUILD_PLATFORM}-windows-v143.7z"
 
 rm -r -f vcpkg/installed/${VCMI_BUILD_PLATFORM}-windows/debug
 mkdir -p vcpkg/installed/${VCMI_BUILD_PLATFORM}-windows/debug/bin
