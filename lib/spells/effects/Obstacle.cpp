edc9812559719a81c2852b5044c8f694d74feddd
Konstantin <ria.freelander@gmail.com>
2023-03-19 21:34:50 +0300

    obstacle: correct logic as in OH3
    Obstacle spells should not be cast if they are affected by native
    terrain and enemy have at least one stack with native terrain

--- lib/spells/effects/Obstacle.cpp
+++ lib/spells/effects/Obstacle.cpp
@@ -121,6 +121,9 @@ void Obstacle::adjustAffectedHexes(std::set<BattleHex> & hexes, const Mechanics 
 
 bool Obstacle::applicable(Problem & problem, const Mechanics * m) const
 {
+	if(hidden && m->battle()->battleHasNativeStack(m->battle()->otherSide(m->casterSide)))
+		return m->adaptProblem(ESpellCastProblem::NO_APPROPRIATE_TARGET, problem);
+
 	return LocationEffect::applicable(problem, m);
 }
 
