f6d91cab35545165c92b9aed2b6fe28f94b807f2
nordsoft <nordsoft@rambler.ru>
2023-04-28 05:34:59 +0400

    Fix portrait serialization
    # Conflicts:
    #	lib/mapObjects/CGHeroInstance.cpp

--- lib/mapObjects/CGHeroInstance.cpp
+++ lib/mapObjects/CGHeroInstance.cpp
@@ -1469,7 +1469,12 @@ void CGHeroInstance::serializeCommonOptions(JsonSerializeFormat & handler)
 			if(portrait >= 0)
 			{
 				if(portrait < legacyHeroes || portrait >= moddedStart)
-					handler.serializeId<si32, si32, HeroTypeID>("portrait", portrait, -1);
+				{
+					int tempPortrait = portrait >= moddedStart
+						? portrait - GameConstants::HERO_PORTRAIT_SHIFT
+						: portrait;
+					handler.serializeId<si32, si32, HeroTypeID>("portrait", tempPortrait, -1);
+				}
 				else
 					handler.serializeInt("portrait", portrait, -1);
 			}
@@ -1479,7 +1484,11 @@ void CGHeroInstance::serializeCommonOptions(JsonSerializeFormat & handler)
 			const JsonNode & portraitNode = handler.getCurrent()["portrait"];
 
 			if(portraitNode.getType() == JsonNode::JsonType::DATA_STRING)
+			{
 				handler.serializeId<si32, si32, HeroTypeID>("portrait", portrait, -1);
+				if(portrait >= legacyHeroes)
+					portrait += GameConstants::HERO_PORTRAIT_SHIFT;
+			}
 			else
 				handler.serializeInt("portrait", portrait, -1);
 		}
