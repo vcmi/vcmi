cbe1c4a07dd3cedf728d5356cefda4f7cb689374
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2023-06-03 08:53:00 +0200

    Remove random seed from map description.

--- lib/rmg/CMapGenerator.cpp
+++ lib/rmg/CMapGenerator.cpp
@@ -164,9 +164,9 @@ std::string CMapGenerator::getMapDescription() const
 		throw rmgException("Map template for Random Map Generator is not found. Could not start the game.");
 
     std::stringstream ss;
-    ss << boost::str(boost::format(std::string("Map created by the Random Map Generator.\nTemplate was %s, Random seed was %d, size %dx%d") +
+    ss << boost::str(boost::format(std::string("Map created by the Random Map Generator.\nTemplate was %s, size %dx%d") +
         ", levels %d, players %d, computers %d, water %s, monster %s, VCMI map") % mapTemplate->getName() %
-		randomSeed % map->width() % map->height() % static_cast<int>(map->levels()) % static_cast<int>(mapGenOptions.getPlayerCount()) %
+		map->width() % map->height() % static_cast<int>(map->levels()) % static_cast<int>(mapGenOptions.getPlayerCount()) %
 		static_cast<int>(mapGenOptions.getCompOnlyPlayerCount()) % waterContentStr[mapGenOptions.getWaterContent()] %
 		monsterStrengthStr[monsterStrengthIndex]);
 
