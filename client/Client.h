982a2723de910f066dfa44a5882e589aa9f46d9f
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2023-06-08 09:49:28 +0200

    Fix missing change

--- client/Client.h
+++ client/Client.h
@@ -228,7 +228,7 @@ public:
 	void giveHeroBonus(GiveBonus * bonus) override {};
 	void setMovePoints(SetMovePoints * smp) override {};
 	void setManaPoints(ObjectInstanceID hid, int val) override {};
-	void giveHero(ObjectInstanceID id, PlayerColor player) override {};
+	void giveHero(ObjectInstanceID id, PlayerColor player, ObjectInstanceID boatId = ObjectInstanceID()) override {};
 	void changeObjPos(ObjectInstanceID objid, int3 newPos) override {};
 	void sendAndApply(CPackForClient * pack) override {};
 	void heroExchange(ObjectInstanceID hero1, ObjectInstanceID hero2) override {};
