3711ce846e36d3b79406948423c927e364ce260c
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2023-07-08 09:11:20 +0200

    Check road settings for connections

--- lib/rmg/modificators/ConnectionsPlacer.cpp
+++ lib/rmg/modificators/ConnectionsPlacer.cpp
@@ -241,7 +241,7 @@ void ConnectionsPlacer::selfSideDirectConnection(const rmg::ZoneConnection & con
 				{
 					rmg::Object monster(*monsterType);
 					monster.setPosition(guardPos);
-					manager.placeObject(monster, false, true, true);
+					manager.placeObject(monster, false, true);
 					//Place objects away from the monster in the other zone, too
 					otherZone->getModificator<ObjectManager>()->updateDistances(monster);
 				}
