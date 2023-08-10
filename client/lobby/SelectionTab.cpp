2d9bb348257744c157ea56081aec9045ed8e58fb
Michael <13953785+Laserlicht@users.noreply.github.com>
2023-08-11 01:49:10 +0200

    format

--- client/lobby/SelectionTab.cpp
+++ client/lobby/SelectionTab.cpp
@@ -357,13 +357,13 @@ void SelectionTab::showPopupWindow(const Point & cursorPosition)
 
 auto SelectionTab::checkSubfolder(std::string path)
 {
-    struct Ret
-    {
-      std::string folderName;
-      std::string baseFolder;
-      bool parentExists;
-      bool fileInFolder;
-    } ret;
+	struct Ret
+	{
+		std::string folderName;
+		std::string baseFolder;
+		bool parentExists;
+		bool fileInFolder;
+	} ret;
 
 	ret.parentExists = (curFolder != "");
 	ret.fileInFolder = false;
@@ -392,7 +392,7 @@ auto SelectionTab::checkSubfolder(std::string path)
 		if(boost::count(pathWithoutPrefix.substr(curFolder.size()), '/') == 0)
 			ret.fileInFolder = true;
 
-    return ret;
+	return ret;
 }
 
 // A new size filter (Small, Medium, ...) has been selected. Populate
