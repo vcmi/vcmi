ac64b9f2b5e797d647960cd8172a41dea71298bc
Michael <13953785+Laserlicht@users.noreply.github.com>
2023-08-17 18:52:00 +0200

    fix

--- client/lobby/OptionsTab.h
+++ client/lobby/OptionsTab.h
@@ -138,7 +138,7 @@ public:
 		void recreate();
 		void setSelection();
 		int getElement(const Point & cursorPosition);
-		void setElement(int element);
+		void setElement(int element, bool apply);
 
 		void clickReleased(const Point & cursorPosition) override;
 		void showPopupWindow(const Point & cursorPosition) override;
