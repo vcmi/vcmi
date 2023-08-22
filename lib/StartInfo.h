a0450d136338c96d839e2e298d4d95bf28b25f86
Michael <13953785+Laserlicht@users.noreply.github.com>
2023-08-23 00:39:09 +0200

    format...

--- lib/StartInfo.h
+++ lib/StartInfo.h
@@ -81,7 +81,7 @@ struct DLL_LINKAGE StartInfo
 	ui32 seedToBeUsed; //0 if not sure (client requests server to decide, will be send in reply pack)
 	ui32 seedPostInit; //so we know that game is correctly synced at the start; 0 if not known yet
 	ui32 mapfileChecksum; //0 if not relevant
-  std::string gameUuid;
+	std::string gameUuid;
 	TurnTimerInfo turnTimerInfo;
 	std::string mapname; // empty for random map, otherwise name of the map or savegame
 	bool createRandomMap() const { return mapGenOptions != nullptr; }
@@ -105,7 +105,7 @@ struct DLL_LINKAGE StartInfo
 		h & seedToBeUsed;
 		h & seedPostInit;
 		h & mapfileChecksum;
-  	h & gameUuid;
+		h & gameUuid;
 		h & turnTimerInfo;
 		h & mapname;
 		h & mapGenOptions;
