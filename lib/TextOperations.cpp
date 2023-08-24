e317c23a594fdb9fe8a01aa07ac91f1bbcb78bf9
Michael <13953785+Laserlicht@users.noreply.github.com>
2023-08-24 20:31:06 +0200

    fix

--- lib/TextOperations.cpp
+++ lib/TextOperations.cpp
@@ -193,7 +193,7 @@ void TextOperations::trimRightUnicode(std::string & text, const size_t amount)
 	}
 }
 
-size_t getUnicodeCharactersCount(const std::string & text)
+size_t TextOperations::getUnicodeCharactersCount(const std::string & text)
 {
 	std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
 	return conv.from_bytes(text).size(); 
