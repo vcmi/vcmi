39fbdd300d461fcddb4dfdd6852e324fe66402b6
nordsoft <nordsoft@rambler.ru>
2023-08-25 14:07:22 +0400

    Refactor combo box

---
+++ client/widgets/ComboBox.cpp
@@ -0,0 +1,8 @@
+//
+//  ComboBox.cpp
+//  vcmiclient
+//
+//  Created by nordsoft on 24.08.2023.
+//
+
+#include "ComboBox.hpp"
---
+++ client/widgets/ComboBox.h
@@ -0,0 +1,13 @@
+//
+//  ComboBox.hpp
+//  vcmiclient
+//
+//  Created by nordsoft on 24.08.2023.
+//
+
+#ifndef ComboBox_hpp
+#define ComboBox_hpp
+
+#include <stdio.h>
+
+#endif /* ComboBox_hpp */
