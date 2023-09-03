96d244a3158e305973992820bfd342725cd6f92a
nordsoft <nordsoft@rambler.ru>
2023-09-03 18:28:22 +0400

    Fix compiling

--- launcher/lobby/chat_moc.cpp
+++ launcher/lobby/chat_moc.cpp
@@ -1,3 +1,13 @@
+/*
+ * chat_moc.cpp, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+#include "StdInc.h"
 #include "chat_moc.h"
 #include "ui_chat_moc.h"
 
