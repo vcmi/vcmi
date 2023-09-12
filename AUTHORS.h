1c68937d95ea1b02cbe29116f858fe661709588a
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2023-09-12 11:52:19 +0200

    new approach

---
+++ AUTHORS.h
@@ -0,0 +1,46 @@
+/*
+ * AUTHORS.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+#pragma once
+
+//VCMI PROJECT CODE CONTRIBUTORS:
+std::vector<std::vector<std::string>> contributors = {
+//   Task          Name                    Aka                      E-Mail
+   { "Developing", "Michał Urbańczyk",     "Tow",                   "impono@gmail.com"             },
+   { "Developing", "Mateusz B.",           "Tow dragon",            "matcio1@gmail.com"            },
+   { "Developing", "Stefan Pavlov",        "Ste",                   "mailste@gmail.com"            },
+   { "Developing", "Yifeng Sun",           "phoebus118",            "pkusunyifeng@gmail.com"       },
+   { "Developing", "Andrea Palmate",       "afxgroup",              "andrea@amigasoft.net"         },
+   { "Developing", "Vadim Glazunov",       "neweagle",              "neweagle@gmail.com"           },
+   { "Developing", "Rafal R.",             "ambtrip",               "ambtrip@wp.pl"                },
+   { "Developing", "Lukasz Wychrystenko",  "tezeriusz",             "t0@czlug.icis.pcz.pl"         },
+   { "Developing", "Xiaomin Ding",         "",                      "dingding303@gmail.com"        },
+   { "Developing", "Tom Zielinski",        "Warmonger",             "Warmonger@vp.pl"              },
+   { "Developing", "Frank Zago",           "ubuntux",               ""                             },
+   { "Developing", "Trevor Standley",      "tstandley",             ""                             },
+   { "Developing", "Rickard Westerlund",   "Onion Knight",          "onionknigh@gmail.com"         },
+   { "Developing", "Ivan Savenko",         "",                      "saven.ivan@gmail.com"         },
+   { "Developing", "Benjamin Gentner",     "beegee",                ""                             },
+   { "Developing", "Alexey ",              "Macron1Robot",          ""                             },
+   { "Developing", "Alexander Shishkin",   "alexvins",              ""                             },
+   { "Developing", "Arseniy Shestakov",    "SXX",                   "me@arseniyshestakov.com"      },
+   { "Developing", "Vadim Markovtsev",     "",                      "gmarkhor@gmail.com"           },
+   { "Developing", "Michał Kalinowski",    "",                      "feniks_fire@o2.pl"            },
+   { "Developing", "",                     "Dydzio",                "blood990@gmail.com"           },
+   { "Developing", "Piotr Wójcik",         "Chocimier",             "chocimier@tlen.pl"            },
+   { "Developing", "Henning Koehler",      "",                      "henning.koehler.nz@gmail.com" },
+   { "Developing", "Andrzej Żak",          "godric3",               ""                             },
+   { "Developing", "Andrii Danylchenko",   "",                      ""                             },
+   { "Developing", "Dmitry Orlov",         "",                      "shubus.corporation@gmail.com" },
+   { "Developing", "Andrey Cherkas",       "nordsoft",              "nordsoft@yahoo.com"           },
+   { "Developing", "",                     "kambala-decapitator",   "decapitator@ukr.net"          },
+   { "Developing", "",                     "Laserlicht",            ""                             },
+   { "Testing",    "",                     "Povelitel",             ""                             },
+   { "Testing",    "",                     "Misiokles",             ""                             },
+};
--- client/mainmenu/CreditsScreen.h
+++ client/mainmenu/CreditsScreen.h
@@ -18,9 +18,6 @@ class CreditsScreen : public CIntObject
 	int positionCounter;
 	std::shared_ptr<CMultiLineLabel> credits;
 
-	// update contributors with bash:   curl -s "https://api.github.com/repos/vcmi/vcmi/contributors" | jq '.[].login' | tr '\n' '|' | sed 's/|/, /g' | sed 's/..$//'
-	std::vector<std::string> contributors = { "IvanSavenko", "alexvins", "mwu-tow", "Nordsoft91", "mateuszbaran", "ArseniyShestakov", "nullkiller", "dydzio0614", "kambala-decapitator", "rilian-la-te", "DjWarmonger", "Laserlicht", "beegee1", "henningkoehlernz", "SoundSSGood", "vmarkovtsev", "krs0", "Mixaill", "Fayth", "ShubusCorporation", "rwesterlund", "Macron1Robot", "Chocimier", "Zyx-2000", "bwrsandman", "stopiccot", "viciious", "karol57", "heroesiiifan", "Adriankhl" };
-
 public:
 	CreditsScreen(Rect rect);
 	void show(Canvas & to) override;
