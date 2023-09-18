bcf32984cebc64714352c44192d04045189e9863
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2023-09-19 01:08:49 +0200

    always sound at chat message

--- client/adventureMap/CInGameConsole.cpp
+++ client/adventureMap/CInGameConsole.cpp
@@ -105,7 +105,13 @@ void CInGameConsole::print(const std::string & txt)
 	}
 
 	GH.windows().totalRedraw(); // FIXME: ingame console has no parent widget set
-	CCS->soundh->playSound(AudioPath::builtin("CHAT"));
+
+	int volume = CCS->soundh->getVolume();
+	if(volume == 0)
+		CCS->soundh->setVolume(settings["general"]["sound"].Integer());
+	int handle = CCS->soundh->playSound(AudioPath::builtin("CHAT"));
+	if(volume == 0)
+		CCS->soundh->setCallback(handle, [&]() { CCS->soundh->setVolume(0); });
 }
 
 bool CInGameConsole::captureThisKey(EShortcut key)
