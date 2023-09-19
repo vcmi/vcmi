2af7d7c08528e454e1acf26afd07854e2c247395
Ivan Savenko <saven.ivan@gmail.com>
2023-09-19 17:02:15 +0300

    Ignore garrison update events of another towns for our castleInt

--- client/windows/CCastleInterface.cpp
+++ client/windows/CCastleInterface.cpp
@@ -1213,7 +1213,12 @@ CCastleInterface::~CCastleInterface()
 
 void CCastleInterface::updateGarrisons()
 {
+	garr->setArmy(town->getUpperArmy(), EGarrisonType::UPPER);
+	garr->setArmy(town->visitingHero, EGarrisonType::LOWER);
 	garr->recreateSlots();
+	heroes->update();
+
+	redraw();
 }
 
 void CCastleInterface::close()
