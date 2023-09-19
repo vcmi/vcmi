371d798302cca06a126176b142ec659347e3abe7
Ivan Savenko <saven.ivan@gmail.com>
2023-09-19 20:09:18 +0300

    Fix tests

--- test/mock/mock_IGameCallback.h
+++ test/mock/mock_IGameCallback.h
@@ -40,8 +40,8 @@ public:
 	void showInfoDialog(const std::string & msg, PlayerColor player) override {}
 
 	void changeSpells(const CGHeroInstance * hero, bool give, const std::set<SpellID> &spells) override {}
-	bool removeObject(const CGObjectInstance * obj) override {return false;}
-	void createObject(const int3 & visitablePosition, Obj type, int32_t subtype = 0) override {};
+	bool removeObject(const CGObjectInstance * obj, const PlayerColor & initiator) override {return false;}
+	void createObject(const int3 & visitablePosition, const PlayerColor & initiator, Obj type, int32_t subtype = 0) override {};
 	void setOwner(const CGObjectInstance * objid, PlayerColor owner) override {}
 	void changePrimSkill(const CGHeroInstance * hero, PrimarySkill which, si64 val, bool abs=false) override {}
 	void changeSecSkill(const CGHeroInstance * hero, SecondarySkill which, int val, bool abs=false) override {}
@@ -83,7 +83,7 @@ public:
 	void setMovePoints(SetMovePoints * smp) override {}
 	void setManaPoints(ObjectInstanceID hid, int val) override {}
 	void giveHero(ObjectInstanceID id, PlayerColor player, ObjectInstanceID boatId = ObjectInstanceID()) override {}
-	void changeObjPos(ObjectInstanceID objid, int3 newPos) override {}
+	void changeObjPos(ObjectInstanceID objid, int3 newPos, const PlayerColor & initiator) override {}
 	void heroExchange(ObjectInstanceID hero1, ObjectInstanceID hero2) override {} //when two heroes meet on adventure map
 	void changeFogOfWar(int3 center, ui32 radius, PlayerColor player, bool hide) override {}
 	void changeFogOfWar(std::unordered_set<int3> &tiles, PlayerColor player, bool hide) override {}
