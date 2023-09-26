e33127a1f74aa1e05b8991593a6d58241ec76bea
Alexander Wilms <f.alexander.wilms@gmail.com>
2023-09-26 17:38:41 +0200

    Launcher setting: Disable spinBoxFramerateLimit if VSync is enabled

--- launcher/settingsView/csettingsview_moc.cpp
+++ launcher/settingsView/csettingsview_moc.cpp
@@ -83,6 +83,7 @@ void CSettingsView::loadSettings()
 
 	ui->spinBoxInterfaceScaling->setValue(settings["video"]["resolution"]["scaling"].Float());
 	ui->spinBoxFramerateLimit->setValue(settings["video"]["targetfps"].Float());
+	ui->spinBoxFramerateLimit->setDisabled(settings["video"]["vsync"].Bool());
 	ui->checkBoxVSync->setChecked(settings["video"]["vsync"].Bool());
 	ui->spinBoxReservedArea->setValue(std::round(settings["video"]["reservedWidth"].Float() * 100));
 
@@ -499,6 +500,7 @@ void CSettingsView::on_checkBoxVSync_stateChanged(int arg1)
 {
 	Settings node = settings.write["video"]["vsync"];
 	node->Bool() = arg1;
+	ui->spinBoxFramerateLimit->setDisabled(settings["video"]["vsync"].Bool());
 }
 
 void CSettingsView::on_comboBoxEnemyPlayerAI_currentTextChanged(const QString &arg1)
