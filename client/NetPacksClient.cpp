bb04ca46b5f46b4be7a3b3ce09dabb3cfcb454b1
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2023-10-28 00:21:16 +0200

    client/NetPacksClient.cpp: Combine null pointer checks
    Co-authored-by: Nordsoft91 <nordsoft@yahoo.com>

--- client/NetPacksClient.cpp
+++ client/NetPacksClient.cpp
@@ -377,13 +377,10 @@ void ApplyFirstClientNetPackVisitor::visitChangeObjPos(ChangeObjPos & pack)
 void ApplyClientNetPackVisitor::visitChangeObjPos(ChangeObjPos & pack)
 {
 	CGObjectInstance *obj = gs.getObjInstance(pack.objid);
-	if(CGI)
+	if(CGI && CGI->mh)
 	{
-		if(CGI->mh)
-		{
-			CGI->mh->onObjectFadeIn(obj, pack.initiator);
-			CGI->mh->waitForOngoingAnimations();
-		}
+		CGI->mh->onObjectFadeIn(obj, pack.initiator);
+		CGI->mh->waitForOngoingAnimations();
 	}
 	cl.invalidatePaths();
 }
