24b5f3f552c7418d469ca209503f8bab017fe85b
Alexander Wilms <f.alexander.wilms@gmail.com>
2024-01-16 21:46:25 +0000

    Fix CQuest.h:101:2: error: 'auto' not allowed in non-static class member

--- lib/mapObjects/CQuest.h
+++ lib/mapObjects/CQuest.h
@@ -98,7 +98,7 @@ public:
 class DLL_LINKAGE IQuestObject
 {
 public:
-	auto * quest = new CQuest();
+	CQuest * quest = new CQuest();
 
 	///Information about quest should remain accessible even if IQuestObject removed from map
 	///All CQuest objects are freed in CMap destructor
