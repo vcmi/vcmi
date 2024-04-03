12da0196a0b4a792eefff58879ca96cdeb4e17a4
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2024-04-03 20:05:35 +0200

    Restrict reindexing to random maps

--- lib/mapping/CMapService.cpp
+++ lib/mapping/CMapService.cpp
@@ -89,7 +89,6 @@ void CMapService::saveMap(const std::unique_ptr<CMap> & map, boost::filesystem::
 		tmp.flush();
 		tmp.close();
 	}
-	map->reindexObjects();
 }
 
 ModCompatibilityInfo CMapService::verifyMapHeaderMods(const CMapHeader & map)
