73ec53a9e00fed0c1ce09e8a4cc31dd1a73b397a
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2024-04-06 15:29:15 +0200

    Fix build

--- lib/mapping/ObstacleProxy.h
+++ lib/mapping/ObstacleProxy.h
@@ -53,7 +53,7 @@ public:
 
 protected:
 	int getWeightedObjects(const int3& tile, CRandomGenerator& rand, IGameCallback * cb, std::list<rmg::Object>& allObjects, std::vector<std::pair<rmg::Object*, int3>>& weightedObjects);
-	void ObstacleProxy::sortObstacles();
+	void sortObstacles();
 
 	rmg::Area blockedArea;
 
