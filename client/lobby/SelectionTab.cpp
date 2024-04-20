1f850b8e4e4e29ddb7c1276de201a8470c3bcf7b
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-04-20 18:57:53 +0200

    fix error with custom campaign

--- client/lobby/SelectionTab.cpp
+++ client/lobby/SelectionTab.cpp
@@ -678,7 +678,7 @@ void SelectionTab::selectFileName(std::string fname)
 	filter(-1);
 	selectAbs(-1);
 
-	if(inputName->getText().empty())
+	if(tabType == ESelectionScreen::saveGame && inputName->getText().empty())
 		inputName->setText("NEWGAME");
 }
 
