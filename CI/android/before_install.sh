61093ab02743d14eac70699bd90649b0902c9029
Andrey Filipenkov <decapitator@ukr.net>
2024-04-22 10:26:05 +0300

    [CI] android adjustments

--- CI/android/before_install.sh
+++ CI/android/before_install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get install ninja-build
+echo "ANDROID_NDK_ROOT=$ANDROID_HOME/ndk/25.2.9519653" >> $GITHUB_ENV
+
+brew install ninja
 
 mkdir ~/.conan ; cd ~/.conan
-curl -L "https://github.com/vcmi/vcmi-dependencies/releases/download/android-1.0/$DEPS_FILENAME.txz" \
+curl -L "https://github.com/vcmi/vcmi-dependencies/releases/download/android-1.1/$DEPS_FILENAME.txz" \
 	| tar -xf - --xz
