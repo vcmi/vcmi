05ced35c58fdc1252bf86bf80dbb823cf1924c1b
Tomasz Zieliński <tomaszzielinskijedenzwielu@gmail.com>
2024-05-05 18:12:38 +0200

    Restore aiValue for banks - used by VCAI

--- lib/mapObjectConstructors/AObjectTypeHandler.h
+++ lib/mapObjectConstructors/AObjectTypeHandler.h
@@ -34,7 +34,7 @@ class DLL_LINKAGE AObjectTypeHandler : public boost::noncopyable
 
 	SObjectSounds sounds;
 
-	std::optional<si32> aiValue;
+	[[deprecated]] std::optional<si32> aiValue;
 	BattleField battlefield;
 
 	std::string modScope;
