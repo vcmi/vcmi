9bdb6f2030ac6ab74b6583bfe1c384c849e9647e
Ivan Savenko <saven.ivan@gmail.com>
2024-05-05 12:24:21 +0300

    Add comment to clarify what needs to be updated

--- CI/linux/before_install.sh
+++ CI/linux/before_install.sh
@@ -3,6 +3,9 @@
 sudo apt-get update
 
 # Dependencies
+# In case of change in dependencies list please also update:
+# - developer docs at docs/developer/Building_Linux.md
+# - debian build settings at debian/control
 sudo apt-get install libboost-dev libboost-filesystem-dev libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-locale-dev libboost-iostreams-dev \
 libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
 qtbase5-dev \
