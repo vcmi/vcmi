4aa69ef345c16ad3b88e5f53bef8f62a17f3a304
Alexander Wilms <f.alexander.wilms@gmail.com>
2024-06-21 01:38:37 +0200

    sort_ui.py: Format with ruff

--- CI/linux-qt6/sort_ui/sort_ui.py
+++ CI/linux-qt6/sort_ui/sort_ui.py
@@ -5,8 +5,9 @@ from lxml import etree
 
 # Inspired from here: https://stackoverflow.com/a/46128043
 
+
 def sort_children(node, klass, key):
-    """ Sort children along tag and given attribute.
+    """Sort children along tag and given attribute.
     if attr is None, sort along all attributes"""
     if not isinstance(node.tag, str):  # PYTHON 2: use basestring instead
         # not a TAG, it is comment or DATA
@@ -45,9 +46,9 @@ def remove_attr(node, attr, value):
         remove_attr(child, attr, value)
 
 
-
-def process(infile, outfile, sort_qgridlayout=True, remove_stdset=True, remove_native=True):
-
+def process(
+    infile, outfile, sort_qgridlayout=True, remove_stdset=True, remove_native=True
+):
     tree = etree.parse(infile)
     root = tree.getroot()
 
@@ -58,44 +59,53 @@ def process(infile, outfile, sort_qgridlayout=True, remove_stdset=True, remove_n
     if remove_native:
         remove_attr(root, "native", "true")
 
-    tree.write(outfile,encoding="UTF-8",xml_declaration=True,pretty_print=True)
+    tree.write(outfile, encoding="UTF-8", xml_declaration=True, pretty_print=True)
 
 
 import argparse
 
-parser = argparse.ArgumentParser(prog="sort_ui",
-                                 description="Qt UI file post processor")
+parser = argparse.ArgumentParser(
+    prog="sort_ui", description="Qt UI file post processor"
+)
 
-parser.add_argument("filenames",
-                    metavar="filename",
-                    type=str,
-                    nargs = "+",
-                    help="The path to the unsorted input file (*.ui). If no outfile is given, the changes will be applied in-place.")
+parser.add_argument(
+    "filenames",
+    metavar="filename",
+    type=str,
+    nargs="+",
+    help="The path to the unsorted input file (*.ui). If no outfile is given, the changes will be applied in-place.",
+)
 
-parser.add_argument("-o",
-                    "--outfile",
-                    metavar="outfile",
-                    type=str,
-                    help="The path to the sorted output file.")
+parser.add_argument(
+    "-o",
+    "--outfile",
+    metavar="outfile",
+    type=str,
+    help="The path to the sorted output file.",
+)
 
-parser.add_argument("--no-sort-qgridlayout",
-                    action="store_false",
-                    dest="sort_qgridlayout",
-                    help="Sort children of QGridLayout by row - column (default: true)")
+parser.add_argument(
+    "--no-sort-qgridlayout",
+    action="store_false",
+    dest="sort_qgridlayout",
+    help="Sort children of QGridLayout by row - column (default: true)",
+)
 
-parser.add_argument("--no-remove-stdset",
-                    action="store_false",
-                    dest="remove_stdset",
-                    help="Do not remove any stdset=\"0\" attribute (default: false)")
+parser.add_argument(
+    "--no-remove-stdset",
+    action="store_false",
+    dest="remove_stdset",
+    help='Do not remove any stdset="0" attribute (default: false)',
+)
 
-parser.add_argument("--no-remove-native",
-                    action="store_false",
-                    dest="remove_native",
-                    help="Do not remove any native=\"true\" attribute (default: false)")
+parser.add_argument(
+    "--no-remove-native",
+    action="store_false",
+    dest="remove_native",
+    help='Do not remove any native="true" attribute (default: false)',
+)
 
-parser.add_argument("--version",
-                    action="version",
-                    version="0.0.2")
+parser.add_argument("--version", action="version", version="0.0.2")
 
 parser.set_defaults(sort_qgridlayout=True, remove_stdset=True, remove_native=True)
 
@@ -106,11 +116,23 @@ args = parser.parse_args()
 if args.outfile is not None:
     if len(args.filenames) == 1:
         filename_out = args.filenames[1]
-        process(filename, filename_out, args.sort_qgridlayout, args.remove_stdset, args.remove_native)
+        process(
+            filename,
+            filename_out,
+            args.sort_qgridlayout,
+            args.remove_stdset,
+            args.remove_native,
+        )
         exit(0)
     else:
         print("-o, --output option can only be used with a single input file")
         exit(1)
 
 for filename in args.filenames:
-    process(filename, filename, args.sort_qgridlayout, args.remove_stdset, args.remove_native)
+    process(
+        filename,
+        filename,
+        args.sort_qgridlayout,
+        args.remove_stdset,
+        args.remove_native,
+    )
