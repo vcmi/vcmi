037ee4395eeef623be93fea4424806ab112913da
Dydzio <blood990@gmail.com>
2024-07-15 20:22:19 +0200

    Allow opening single hero when two heroes in town via shift+click

--- client/windows/CCastleInterface.cpp
+++ client/windows/CCastleInterface.cpp
@@ -428,7 +428,7 @@ void CHeroGSlot::clickPressed(const Point & cursorPosition)
 	{
 		setHighlight(false);
 
-		if(other->hero)
+		if(other->hero && !GH.isKeyboardShiftDown())
 			LOCPLINT->showHeroExchange(hero->id, other->hero->id);
 		else
 			LOCPLINT->openHeroWindow(hero);
