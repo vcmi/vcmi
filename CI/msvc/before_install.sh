8bca2fa6439331884a4dcab1418e5f4f3bdac4f0
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2024-07-19 23:09:35 +0200

    MSVC CI script: Don't run vswhere to find dumpbin.exe, since it is not used
    Just the search took 3 to 15 min

--- CI/msvc/before_install.sh
+++ CI/msvc/before_install.sh
@@ -5,6 +5,3 @@ curl -LfsS -o "vcpkg-export-${VCMI_BUILD_PLATFORM}-windows-v143.7z" \
 #rm -r -f vcpkg/installed/${VCMI_BUILD_PLATFORM}-windows/debug
 #mkdir -p vcpkg/installed/${VCMI_BUILD_PLATFORM}-windows/debug/bin
 #cp vcpkg/installed/${VCMI_BUILD_PLATFORM}-windows/bin/* vcpkg/installed/${VCMI_BUILD_PLATFORM}-windows/debug/bin
-
-DUMPBIN_DIR=$(vswhere -latest -find **/dumpbin.exe | head -n 1)
-dirname "$DUMPBIN_DIR" > $GITHUB_PATH
