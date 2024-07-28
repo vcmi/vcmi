ec3a9ca8a6c206e3f3b53f9a90166e7abd9cc999
Alexander Wilms <Alexander-Wilms@users.noreply.github.com>
2024-07-28 00:05:13 +0000

    CI: Add clang-tidy.yml copied from feroes

--- CI/linux-qt6/before_install.sh
+++ CI/linux-qt6/before_install.sh
@@ -8,4 +8,4 @@ sudo apt-get install libboost-dev libboost-filesystem-dev libboost-system-dev li
 libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
 qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools qt6-l10n-tools \
 ninja-build zlib1g-dev libavformat-dev libswscale-dev libtbb-dev libluajit-5.1-dev \
-libminizip-dev libfuzzylite-dev libsqlite3-dev # Optional dependencies
+libminizip-dev libfuzzylite-dev libsqlite3-dev clang-tidy-15 # Optional dependencies
