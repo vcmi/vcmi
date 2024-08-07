d5579a92d55ea0392a85484e9696516287a3e37a
MichalZr6 <zarembamichal@aol.com>
2024-08-07 19:36:16 +0200

    Fixes based on review remarks

--- lib/mapObjectConstructors/HillFortInstanceConstructor.cpp
+++ lib/mapObjectConstructors/HillFortInstanceConstructor.cpp
@@ -11,17 +11,27 @@
 #include "HillFortInstanceConstructor.h"
 
 #include "../mapObjects/MiscObjects.h"
+#include "../texts/CGeneralTextHandler.h"
 
 VCMI_LIB_NAMESPACE_BEGIN
 
 void HillFortInstanceConstructor::initTypeData(const JsonNode & config)
 {
 	parameters = config;
+	VLC->generaltexth->registerString(parameters.getModScope(), TextIdentifier(getBaseTextID(), "unavailableUpgradeMessage"), parameters["unavailableUpgradeMessage"].String());
+	VLC->generaltexth->registerString(parameters.getModScope(), TextIdentifier(getBaseTextID(), "description"), parameters["description"].String());
 }
 
 void HillFortInstanceConstructor::initializeObject(HillFort * fort) const
 {
 	fort->upgradeCostPercentage = parameters["upgradeCostFactor"].convertTo<std::vector<int>>();
+	fort->descriptionToolTip = VLC->generaltexth->translate(TextIdentifier(getBaseTextID(), "description").get());
+	if (fort->descriptionToolTip.empty())
+		fort->descriptionToolTip = parameters["description"].String();		
+
+	fort->unavailableUpgradeMessage = VLC->generaltexth->translate(TextIdentifier(getBaseTextID(), "unavailableUpgradeMessage").get());
+	if (fort->unavailableUpgradeMessage.empty())
+		fort->unavailableUpgradeMessage = parameters["unavailableUpgradeMessage"].String();
 }
 
 VCMI_LIB_NAMESPACE_END
