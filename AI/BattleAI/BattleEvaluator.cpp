d09fb07362e96666fac42de133bd8753a350245e
Ivan Savenko <saven.ivan@gmail.com>
2024-08-15 13:14:51 +0000

    Use throwing ::at to prevent undefined behavior

--- AI/BattleAI/BattleEvaluator.cpp
+++ AI/BattleAI/BattleEvaluator.cpp
@@ -265,7 +265,7 @@ BattleAction BattleEvaluator::goTowardsNearest(const CStack * stack, std::vector
 	{
 		std::sort(targetHexes.begin(), targetHexes.end(), [&](BattleHex h1, BattleHex h2) -> bool
 			{
-				return reachability.distances[h1] < reachability.distances[h2];
+				return reachability.distances.at(h1) < reachability.distances.at(h2);
 			});
 
 		for(auto hex : targetHexes)
@@ -283,7 +283,7 @@ BattleAction BattleEvaluator::goTowardsNearest(const CStack * stack, std::vector
 			}
 		}
 
-		if(reachability.distances[targetHexes.front()] <= GameConstants::BFIELD_SIZE)
+		if(reachability.distances.at(targetHexes.front()) <= GameConstants::BFIELD_SIZE)
 		{
 			break;
 		}
@@ -299,7 +299,7 @@ BattleAction BattleEvaluator::goTowardsNearest(const CStack * stack, std::vector
 
 	BattleHex bestNeighbor = targetHexes.front();
 
-	if(reachability.distances[bestNeighbor] > GameConstants::BFIELD_SIZE)
+	if(reachability.distances.at(bestNeighbor) > GameConstants::BFIELD_SIZE)
 	{
 		return BattleAction::makeDefend(stack);
 	}
