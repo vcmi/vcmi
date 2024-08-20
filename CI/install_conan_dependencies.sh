e70ab8699530951cf0d1c24a9e91fff5ea63c9db
Ivan Savenko <saven.ivan@gmail.com>
2024-08-20 20:37:49 +0000

    Use .txz archive for dependencies

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -2,8 +2,8 @@
 
 RELEASE_TAG="1.2"
 FILENAME="$1"
-DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME.tgz"
+DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME.txz"
 
 mkdir ~/.conan
 cd ~/.conan
-curl -L "$DOWNLOAD_URL" | tar -xzf -
+curl -L "$DOWNLOAD_URL" | tar -xf - --xz
