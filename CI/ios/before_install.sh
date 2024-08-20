1ac63056dcfdcd835560d8d77d618cd3395c66a1
Ivan Savenko <saven.ivan@gmail.com>
2024-08-20 18:34:52 +0000

    Unify code for conan dependencies installation

--- CI/ios/before_install.sh
+++ CI/ios/before_install.sh
@@ -2,6 +2,4 @@
 
 echo DEVELOPER_DIR=/Applications/Xcode_14.2.app >> $GITHUB_ENV
 
-mkdir ~/.conan ; cd ~/.conan
-curl -L 'https://github.com/vcmi/vcmi-dependencies/releases/download/1.2/dependencies-ios.tgz' \
-	| tar -xzf -
+. CI/install_conan_dependencies.sh "dependencies-ios"
--- CI/mac/before_install.sh
+++ CI/mac/before_install.sh
@@ -4,6 +4,4 @@ echo DEVELOPER_DIR=/Applications/Xcode_14.2.app >> $GITHUB_ENV
 
 brew install ninja
 
-mkdir ~/.conan ; cd ~/.conan
-curl -L "https://github.com/vcmi/vcmi-dependencies/releases/download/1.2/$DEPS_FILENAME.tgz" \
-	| tar -xzf -
+. CI/install_conan_dependencies.sh "$DEPS_FILENAME"
