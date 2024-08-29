1623afd35d23f03c4fa1e11e9ba6c265b8017573
Ivan Savenko <saven.ivan@gmail.com>
2024-08-29 19:03:51 +0000

    Use Ubuntu 24.04 for mingw builds

--- CI/mingw/before_install.sh
+++ CI/mingw/before_install.sh
@@ -3,12 +3,3 @@
 sudo apt-get update
 sudo apt-get install ninja-build mingw-w64 nsis
 sudo update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
-
-# Workaround for getting new MinGW headers on Ubuntu 22.04.
-# Remove it once MinGW headers version in repository will be 10.0 at least
-curl -O -L http://mirrors.kernel.org/ubuntu/pool/universe/m/mingw-w64/mingw-w64-common_10.0.0-3_all.deb \
-  && sudo dpkg -i mingw-w64-common_10.0.0-3_all.deb;
-curl -O -L http://mirrors.kernel.org/ubuntu/pool/universe/m/mingw-w64/mingw-w64-x86-64-dev_10.0.0-3_all.deb \
-  && sudo dpkg -i mingw-w64-x86-64-dev_10.0.0-3_all.deb;
-
-. CI/install_conan_dependencies.sh "dependencies-mingw"
