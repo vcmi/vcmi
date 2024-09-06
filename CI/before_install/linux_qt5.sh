ecf063cd1c86a7a34b2a28d803de96e2cb83c74b
Ivan Savenko <saven.ivan@gmail.com>
2024-09-06 16:54:49 +0000

    Reorganized layout of CI directory, move all before_install files into common directory, remove copy-pasted files

--- CI/before_install/linux_qt5.sh
+++ CI/before_install/linux_qt5.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-sudo apt remove needrestart
 sudo apt-get update
 
 # Dependencies
+# In case of change in dependencies list please also update:
+# - developer docs at docs/developer/Building_Linux.md
+# - debian build settings at debian/control
 sudo apt-get install libboost-dev libboost-filesystem-dev libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-locale-dev libboost-iostreams-dev \
 libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
-qt6-base-dev qt6-base-dev-tools qt6-tools-dev qt6-tools-dev-tools qt6-l10n-tools \
+qtbase5-dev qttools5-dev \
 ninja-build zlib1g-dev libavformat-dev libswscale-dev libtbb-dev libluajit-5.1-dev \
 libminizip-dev libfuzzylite-dev libsqlite3-dev # Optional dependencies
