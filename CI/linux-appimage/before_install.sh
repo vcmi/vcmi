ab64c097791319c97a3c90b2285a30847add1b4b
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2024-09-07 04:48:44 +0200

    appimage support

---
+++ CI/linux-appimage/before_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt remove needrestart
+sudo apt-get update
+
+# Dependencies
+# In case of change in dependencies list please also update:
+# - developer docs at docs/developer/Building_Linux.md
+# - debian build settings at debian/control
+sudo apt-get install libboost-dev libboost-filesystem-dev libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-locale-dev libboost-iostreams-dev \
+libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
+qtbase5-dev \
+ninja-build zlib1g-dev libavformat-dev libswscale-dev libtbb-dev libluajit-5.1-dev \
+libminizip-dev libfuzzylite-dev qttools5-dev libsqlite3-dev # Optional dependencies
