6b1fd428bd016f221ca22f2500134a6fbe4a983f
Ivan Savenko <saven.ivan@gmail.com>
2024-09-19 17:04:40 +0000

    Use shared setup file for 32 and 64 bit bingw

--- CI/before_install/mingw.sh
+++ CI/before_install/mingw.sh
@@ -2,4 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install ninja-build mingw-w64 nsis
+
 sudo update-alternatives --set i686-w64-mingw32-g++ /usr/bin/i686-w64-mingw32-g++-posix
+sudo update-alternatives --set x86_64-w64-mingw32-g++ /usr/bin/x86_64-w64-mingw32-g++-posix
