eb94c9f0bee4a7e165fa51539ee6800035c00455
Xilmi <ail.st@gmx.de>
2024-10-10 18:53:28 +0200

    Update PriorityEvaluator.cpp
    AI will be more aggressive when defending their territory. And more aggressive means more willing to take losses while fighting.

--- AI/Nullkiller/Engine/PriorityEvaluator.cpp
+++ AI/Nullkiller/Engine/PriorityEvaluator.cpp
@@ -1345,7 +1345,7 @@ float PriorityEvaluator::evaluate(Goals::TSubgoal task, int priorityTier)
 	else
 	{
 		float score = 0;
-		float maxWillingToLose = ai->cb->getTownsInfo().empty() ? 1 : 0.25;
+		float maxWillingToLose = ai->cb->getTownsInfo().empty() || (evaluationContext.isDefend && evaluationContext.threatTurns == 0) ? 1 : 0.25;
 
 		bool arriveNextWeek = false;
 		if (ai->cb->getDate(Date::DAY_OF_WEEK) + evaluationContext.turn > 7)
