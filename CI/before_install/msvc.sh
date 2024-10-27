dace4075e424a54cf9de787e8aade33d88d41da6
Ivan Savenko <saven.ivan@gmail.com>
2024-10-27 17:45:12 +0000

    Changes according to review

--- CI/before_install/msvc.sh
+++ CI/before_install/msvc.sh
@@ -3,14 +3,13 @@
 MSVC_INSTALL_PATH=$(vswhere -latest -property installationPath)
 echo "MSVC_INSTALL_PATH = $MSVC_INSTALL_PATH"
 echo "Installed toolset versions:"
-ls -vr "$MSVC_INSTALL_PATH"/VC/Tools/MSVC
+ls -vr "$MSVC_INSTALL_PATH/VC/Tools/MSVC"
 
-TOOLS_DIR=$(ls -vr "$MSVC_INSTALL_PATH"/VC/Tools/MSVC/ | head -1)
-DUMPBIN_PATH="$MSVC_INSTALL_PATH"/VC/Tools/MSVC/"$TOOLS_DIR"/bin/Hostx64/x64/dumpbin.exe
+TOOLS_DIR=$(ls -vr "$MSVC_INSTALL_PATH/VC/Tools/MSVC/" | head -1)
+DUMPBIN_PATH="$MSVC_INSTALL_PATH/VC/Tools/MSVC/$TOOLS_DIR/bin/Hostx64/x64/dumpbin.exe"
 
-# This script should also work, but for some reason is *extremely* slow on Github CI (~7 minutes)
+# This command should work as well, but for some reason it is *extremely* slow on the Github CI (~7 minutes)
 #DUMPBIN_PATH=$(vswhere -latest -find **/dumpbin.exe | head -n 1)
-#
 
 echo "TOOLS_DIR = $TOOLS_DIR"
 echo "DUMPBIN_PATH = $DUMPBIN_PATH"
