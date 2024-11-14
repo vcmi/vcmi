ab1f1d6d99853861d1b1a81dcd588ec970f1db6e
MichalZr6 <zarembamichal@aol.com>
2024-11-15 00:58:35 +0100

    Fix crash on miniHillFort popup window

--- lib/mapObjects/MiscObjects.cpp
+++ lib/mapObjects/MiscObjects.cpp
@@ -1337,7 +1337,7 @@ std::string HillFort::getPopupText(PlayerColor player) const
 {
 	MetaString message = MetaString::createFromRawString("{%s}\r\n\r\n%s");
 
-	message.replaceName(ID);
+	message.replaceName(ID, subID);
 	message.replaceTextID(getDescriptionToolTip());
 
 	return message.toString();
