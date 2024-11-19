f823b8addfa97666ad211044f9b964c52c52fee3
MichalZr6 <zarembamichal@aol.com>
2024-11-19 10:57:20 +0100

    Following review

--- lib/mapObjectConstructors/HillFortInstanceConstructor.cpp
+++ lib/mapObjectConstructors/HillFortInstanceConstructor.cpp
@@ -18,7 +18,7 @@ VCMI_LIB_NAMESPACE_BEGIN
 void HillFortInstanceConstructor::initTypeData(const JsonNode & config)
 {
 	parameters = config;
-	if(getModScope() != "core")
+	if(!parameters["unavailableUpgradeMessage"].isNull())
 		VLC->generaltexth->registerString(parameters.getModScope(), TextIdentifier(getBaseTextID(), "unavailableUpgradeMessage"), parameters["unavailableUpgradeMessage"].String());
 	
 	VLC->generaltexth->registerString(parameters.getModScope(), TextIdentifier(getBaseTextID(), "description"), parameters["description"].String());
