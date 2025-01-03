eeb2ea654788ae17915ee14eb26d2eeef6f01edc
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2025-01-03 23:23:52 +0100

    Fix seed condition

--- mapeditor/windownewmap.cpp
+++ mapeditor/windownewmap.cpp
@@ -296,7 +296,7 @@ void WindowNewMap::on_okButton_clicked()
 		}
 		
 		int seed = std::time(nullptr);
-		if(ui->checkSeed->isChecked() && !ui->lineSeed->value() != 0)
+		if(ui->checkSeed->isChecked() && ui->lineSeed->value() != 0)
 			seed = ui->lineSeed->value();
 			
 		CMapGenerator generator(mapGenOptions, nullptr, seed);
