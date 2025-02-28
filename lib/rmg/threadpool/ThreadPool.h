38b537dcaf03c0fd3a71be8388d15d47ea4c90d9
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2025-02-28 16:29:44 +0100

    remove boost header

--- lib/rmg/threadpool/ThreadPool.h
+++ lib/rmg/threadpool/ThreadPool.h
@@ -27,7 +27,7 @@ private:
 	using Lock = std::unique_lock<std::shared_mutex>;
 	mutable std::shared_mutex mx;
 	mutable std::condition_variable_any cv;
-	mutable boost::once_flag once;
+	mutable std::once_flag once;
 
 	bool isInitialized = false;
 	bool stopping = false;
@@ -56,8 +56,7 @@ private:
 	mutable BlockingQueue<TRMGfunction> tasks;
 };
 
-ThreadPool::ThreadPool() :
-	once(BOOST_ONCE_INIT)
+ThreadPool::ThreadPool()
 {};
 
 ThreadPool::~ThreadPool()
@@ -67,7 +66,7 @@ ThreadPool::~ThreadPool()
 
 inline void ThreadPool::init(size_t numThreads)
 {
-	boost::call_once(once, [this, numThreads]()
+	std::call_once(once, [this, numThreads]()
 	{
 		Lock lock(mx);
 		stopping = false;
