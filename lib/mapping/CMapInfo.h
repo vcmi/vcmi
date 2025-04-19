e308b2149aaff3a5b8500f16c1da6152886e2992
kdmcser <zqtndfj@sina.com>
2025-04-19 13:44:48 +0800

    fix compile error

--- lib/mapping/CMapInfo.h
+++ lib/mapping/CMapInfo.h
@@ -48,7 +48,7 @@ public:
 	CMapInfo &operator=(CMapInfo &&other) = delete;
 	CMapInfo &operator=(const CMapInfo &other) = delete;
 
-	std::string CMapInfo::getFullFileURI(const ResourcePath& file) const;
+	std::string getFullFileURI(const ResourcePath& file) const;
 	void mapInit(const std::string & fname);
 	void saveInit(const ResourcePath & file);
 	void campaignInit();
