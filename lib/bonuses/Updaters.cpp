a17f59b704ece879ffcdcc5e8b43940d243b97e4
kdmcser <zqtndfj@sina.com>
2025-04-26 02:15:25 +0800

    fix crash when bonus limiter is empty in OwnerUpdater

--- lib/bonuses/Updaters.cpp
+++ lib/bonuses/Updaters.cpp
@@ -229,7 +229,8 @@ std::shared_ptr<Bonus> OwnerUpdater::createUpdatedBonus(const std::shared_ptr<Bo
 	std::shared_ptr<Bonus> updated =
 		std::make_shared<Bonus>(*b);
 	updated->limiter = b->limiter;
-	updated->limiter->acceptUpdater(*this);
+	if (updated->limiter)
+		updated->limiter->acceptUpdater(*this);
 	return updated;
 }
 
