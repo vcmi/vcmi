ec8edd4edd87932ed4f57ae00495d12fa1e6c874
Andrey Filipenkov <decapitator@ukr.net>
2025-06-11 21:36:18 +0300

    [CI] adjustments to Conan v2

--- CI/before_install/msvc.sh
+++ CI/before_install/msvc.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-MSVC_INSTALL_PATH=$(vswhere -latest -property installationPath)
-echo "MSVC_INSTALL_PATH = $MSVC_INSTALL_PATH"
-echo "Installed toolset versions:"
-ls -vr "$MSVC_INSTALL_PATH/VC/Tools/MSVC"
+RELEASE_TAG="v1.9"
+UCRT_FILENAME="ucrtRedist-$1"
+UCRT_URL="https://github.com/vcmi/vcmi-deps-windows/releases/download/$RELEASE_TAG/$UCRT_FILENAME.txz"
 
-TOOLS_DIR=$(ls -vr "$MSVC_INSTALL_PATH/VC/Tools/MSVC/" | head -1)
-DUMPBIN_PATH="$MSVC_INSTALL_PATH/VC/Tools/MSVC/$TOOLS_DIR/bin/Hostx64/x64/dumpbin.exe"
+UCRT_DIR="$GITHUB_WORKSPACE/ucrt"
+echo "UCRT_DIR=$UCRT_DIR" >> $GITHUB_ENV
 
-# This command should work as well, but for some reason it is *extremely* slow on the Github CI (~7 minutes)
-#DUMPBIN_PATH=$(vswhere -latest -find **/dumpbin.exe | head -n 1)
-
-echo "TOOLS_DIR = $TOOLS_DIR"
-echo "DUMPBIN_PATH = $DUMPBIN_PATH"
-
-dirname "$DUMPBIN_PATH" > "$GITHUB_PATH"
+mkdir -p "$UCRT_DIR"
+curl -L "$UCRT_URL" | tar -xf - --xz -C "$UCRT_DIR"
