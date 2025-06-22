36420794c7fa125f3c5c47b82727596ad0cdcb82
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2025-06-22 16:49:28 +0200

    translation of credit titles from HoMM3

--- client/mainmenu/CreditsScreen.cpp
+++ client/mainmenu/CreditsScreen.cpp
@@ -47,6 +47,37 @@ CreditsScreen::CreditsScreen(Rect rect)
 	std::string text((char *)textFile.first.get(), textFile.second);
 	size_t firstQuote = text.find('\"') + 1;
 	text = text.substr(firstQuote, text.find('\"', firstQuote) - firstQuote);
+
+	auto translateCredits = [&text](std::map<std::string, std::string> replacements){
+		for(auto & item : replacements)
+			boost::replace_first(text, "{" + item.second + ":}", "{" + LIBRARY->generaltexth->translate(item.first) + ":}");
+	};
+	translateCredits({
+		{ "core.credits.createdBy",           "Created By"           },
+		{ "core.credits.executiveProducer",   "Executive Producer"   },
+		{ "core.credits.producer",            "Producer"             },
+		{ "core.credits.director",            "Director"             },
+		{ "core.credits.designers",           "Designers"            },
+		{ "core.credits.leadProgrammers",     "Lead Programmers"     },
+		{ "core.credits.programmers",         "Programmers"          },
+		{ "core.credits.installerProgrammer", "Installer Programmer" },
+		{ "core.credits.leadArtists",         "Lead Artists"         },
+		{ "core.credits.artists",             "Artists"              },
+		{ "core.credits.assetCoordinator",    "Asset Coordinator"    },
+		{ "core.credits.levelDesigners",      "Level Designers"      },
+		{ "core.credits.musicProducer",       "Music Producer"       },
+		{ "core.credits.townThemes",          "Town Themes"          },
+		{ "core.credits.music",               "Music"                },
+		{ "core.credits.soundDesign",         "Sound Design"         },
+		{ "core.credits.voiceProduction",     "Voice Production"     },
+		{ "core.credits.voiceTalent",         "Voice Talent"         },
+		{ "core.credits.leadTester",          "Lead Tester"          },
+		{ "core.credits.seniorTester",        "Senior Tester"        },
+		{ "core.credits.testers",             "Testers"              },
+		{ "core.credits.specialThanks",       "Special Thanks"       },
+		{ "core.credits.visitUsOnTheWeb",     "Visit us on the Web"  }
+	});
+
 	text = "{- " + LIBRARY->generaltexth->translate("vcmi.credits.vcmi") + " -}\r\n" + contributorsText + "\r\n\r\n{" + LIBRARY->generaltexth->translate("vcmi.credits.website") + ":}\r\nhttps://vcmi.eu\r\n\r\n\r\n\r\n\r\n{- " + LIBRARY->generaltexth->translate("vcmi.credits.heroes") + " -}\r\n\r\n\r\n" + text;
 	credits = std::make_shared<CMultiLineLabel>(Rect(pos.w - 350, 0, 350, 600), FONT_CREDITS, ETextAlignment::CENTER, Colors::WHITE, text);
 	credits->scrollTextTo(-600); // move all text below the screen
