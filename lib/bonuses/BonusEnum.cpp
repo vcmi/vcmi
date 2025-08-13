56fd8893c5d92a11968238e167503019dd3b5755
Opuszek <panopuszek@gmail.com>
2025-08-13 22:16:10 +0200

    Fixes, Fixes

--- lib/bonuses/BonusEnum.cpp
+++ lib/bonuses/BonusEnum.cpp
@@ -22,7 +22,7 @@ VCMI_LIB_NAMESPACE_BEGIN
 	const std::vector<BonusType> bonusAffectingCreature = {
 		BONUS_AFFECTING_CREATURE
 	};
-#undef BONUS_AFFECTING_CREATURE
+#undef BONUS_NAME
 
 #define BONUS_SOURCE(x) { #x, BonusSource::x },
 	const std::map<std::string, BonusSource> bonusSourceMap = { BONUS_SOURCE_LIST };
