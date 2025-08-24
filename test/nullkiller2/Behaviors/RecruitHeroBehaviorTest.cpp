39b24740514ab7d916227a2aaeae66d0a8d67886
Mircea TheHonestCTO <mircea@thehonestcto.com>
2025-08-24 12:12:08 +0200

    fix RecruitHeroBehaviorTest.cpp #include

--- test/nullkiller2/Behaviors/RecruitHeroBehaviorTest.cpp
+++ test/nullkiller2/Behaviors/RecruitHeroBehaviorTest.cpp
@@ -8,8 +8,11 @@
  *
  */
 
-#include "AI/Nullkiller2/Behaviors/RecruitHeroBehavior.h"
-#include "AI/Nullkiller2/Engine/Nullkiller.h"
+#include <Global.h>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <Behaviors/RecruitHeroBehavior.h>
+#include <Engine/Nullkiller.h>
 
 class MockNullkiller : public NK2AI::Nullkiller
 {
