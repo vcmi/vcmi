d085241007b3d4711af4f988ab2d129699a364d4
Mircea TheHonestCTO <mircea@thehonestcto.com>
2025-09-06 05:25:00 +0200

    fix race condition between MapViewController::hasOngoingAnimations and waitForOngoingAnimations internal usage

--- client/mapView/MapViewController.cpp
+++ client/mapView/MapViewController.cpp
@@ -580,19 +580,9 @@ void MapViewController::onHeroMoved(const CGHeroInstance * obj, const int3 & fro
 
 bool MapViewController::hasOngoingAnimations()
 {
-	if(movementContext)
-		return true;
-
-	if(fadingOutContext)
-		return true;
-
-	if(fadingInContext)
-		return true;
-
-	if(teleportContext)
-		return true;
-
-	return false;
+	// We need to be consistent with waitForOngoingAnimations, to check against the same thing here as there,
+	// otherwise we can have tricky race conditions, as we used to before this change
+	return animationWait.isBusy();
 }
 
 void MapViewController::waitForOngoingAnimations()
