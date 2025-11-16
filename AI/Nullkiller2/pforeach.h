69d24111df8324b7f3528561163232f1c26e3204
Mircea TheHonestCTO <mircea@thehonestcto.com>
2025-11-16 17:55:33 +0100

    nullkiller2: remove commented out code, plus formatting the affected file

--- AI/Nullkiller2/pforeach.h
+++ AI/Nullkiller2/pforeach.h
@@ -6,45 +6,28 @@
 namespace NK2AI
 {
 
-// template<typename TFunc>
-// void pforeachTilePos(const int3 & mapSize, TFunc fn)
-// {
-// 	for(int z = 0; z < mapSize.z; ++z)
-// 	{
-// 		tbb::parallel_for(tbb::blocked_range<size_t>(0, mapSize.x), [&](const tbb::blocked_range<size_t> & r)
-// 			{
-// 				int3 pos(0, 0, z);
-//
-// 				for(pos.x = r.begin(); pos.x != r.end(); ++pos.x)
-// 				{
-// 					for(pos.y = 0; pos.y < mapSize.y; ++pos.y)
-// 					{
-// 						fn(pos);
-// 					}
-// 				}
-// 			});
-// 	}
-// }
-
 template<typename TFunc>
 void pforeachTilePaths(const int3 & mapSize, const Nullkiller * aiNk, TFunc fn)
 {
 	for(int z = 0; z < mapSize.z; ++z)
 	{
-		tbb::parallel_for(tbb::blocked_range<size_t>(0, mapSize.x), [&](const tbb::blocked_range<size_t> & r)
-		{
-			int3 pos(0, 0, z);
-			std::vector<AIPath> paths;
-
-			for(pos.x = r.begin(); pos.x != r.end(); ++pos.x)
+		tbb::parallel_for(
+			tbb::blocked_range<size_t>(0, mapSize.x),
+			[&](const tbb::blocked_range<size_t> & r)
 			{
-				for(pos.y = 0; pos.y < mapSize.y; ++pos.y)
+				int3 pos(0, 0, z);
+				std::vector<AIPath> paths;
+
+				for(pos.x = r.begin(); pos.x != r.end(); ++pos.x)
 				{
-					aiNk->pathfinder->calculatePathInfo(paths, pos);
-					fn(pos, paths);
+					for(pos.y = 0; pos.y < mapSize.y; ++pos.y)
+					{
+						aiNk->pathfinder->calculatePathInfo(paths, pos);
+						fn(pos, paths);
+					}
 				}
 			}
-		});
+		);
 	}
 }
 
