ede1668395f2ebe0dab2d76236db8b5c3aff9b04
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2025-11-29 15:30:04 +0100

    Templateeditor: Support forced level

--- mapeditor/templateeditor/templateeditor.h
+++ mapeditor/templateeditor/templateeditor.h
@@ -58,6 +58,7 @@ private slots:
 	void on_doubleSpinBoxZoneVisSize_valueChanged();
 	void on_comboBoxZoneType_currentTextChanged(const QString &text);
 	void on_comboBoxZoneOwner_currentTextChanged(const QString &text);
+	void on_comboBoxForcedLevel_currentTextChanged(const QString &text);
 	void on_spinBoxZoneSize_valueChanged();
 	void on_spinBoxTownCountPlayer_valueChanged();
 	void on_spinBoxCastleCountPlayer_valueChanged();
