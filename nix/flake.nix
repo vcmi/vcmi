892d7c6b442d1d2cd78b9162168b2afd3179e9bf
Balint66 <44706571+Balint66@users.noreply.github.com>
2025-12-28 01:24:44 +0100

    Format and modularise

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,14 +4,38 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs@{flake-parts, ... } :
-  flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-        "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
-    ];
+  #My beloved submodules...
+  inputs.self.submodules = true;
+  outputs = inputs @ {
+    flake-parts,
+    self,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-    perSystem = { config, self', inputs', pkgs, system, ... }: {
-      devShells.default = import ./shell.nix { inherit pkgs; };
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = import ./shell.nix {
+          inherit pkgs;
+          ninja = pkgs.ninja;
+        };
+        packages.default = pkgs.stdenv.mkDerivation (import ./default.nix {
+          inherit pkgs;
+          ninja = pkgs.ninja;
+        });
+        formatter = pkgs.alejandra;
+      };
     };
-  };
 }
--- nix/libsquish.nix
+++ nix/libsquish.nix
@@ -4,20 +4,24 @@
   source ? null,
   version ? "1.15",
   cmake,
-  git
-} : let
-  _source = (if source != null then source else fetchsvn {
-    url="svn://svn.code.sf.net/p/libsquish/code/trunk";
-    rev = "r111";
-    sha256 = "sha256-LMIpTIvFkGbBs3Ns2oVuIz3ulk/9DG0KMn8wNSJctEk=";
-  });
-in stdenv.mkDerivation {
-  inherit version;
-  pname = "libsquish";
-  src = _source;
-  buildInputs = [  ];
-  nativeBuildInputs = [ cmake git ];
-  doCheck = true;
-  cmakeFlags = ["-DBUILD_SHARED_LIBS=ON"];
-
-}
+  git,
+}: let
+  _source =
+    if source != null
+    then source
+    else
+      fetchsvn {
+        url = "svn://svn.code.sf.net/p/libsquish/code/trunk";
+        rev = "r111";
+        sha256 = "sha256-LMIpTIvFkGbBs3Ns2oVuIz3ulk/9DG0KMn8wNSJctEk=";
+      };
+in
+  stdenv.mkDerivation {
+    inherit version;
+    pname = "libsquish";
+    src = _source;
+    buildInputs = [];
+    nativeBuildInputs = [cmake git];
+    doCheck = true;
+    cmakeFlags = ["-DBUILD_SHARED_LIBS=ON"];
+  }
