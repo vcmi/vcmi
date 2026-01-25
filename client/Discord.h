adb90e0b8a8d4758100e9b18b1557ab9a4a7b117
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2026-01-25 03:30:31 +0100

    discord integration

---
+++ client/Discord.h
@@ -0,0 +1,29 @@
+/*
+ * Discord.h, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+#pragma once
+
+#include "StdInc.h"
+
+VCMI_LIB_NAMESPACE_BEGIN
+struct StartInfo;
+class CMap;
+VCMI_LIB_NAMESPACE_END
+
+class Discord
+{
+    time_t startTime = time(nullptr);
+public:
+    Discord();
+    ~Discord();
+    void setStatus(std::string state, std::string details, std::tuple<int, int> partySize);
+    void clearStatus();
+
+    void setPlayingStatus(std::shared_ptr<StartInfo> si, const CMap * map, int humanInterfacesCount);
+};
