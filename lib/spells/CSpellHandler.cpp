faa97e463a5e0ba359ae83e840f5c7fe3968938d
Ye Joo Oh <yejoooh94@gmail.com>
2026-02-10 08:29:39 -0800

    making changes

--- lib/spells/CSpellHandler.cpp
+++ lib/spells/CSpellHandler.cpp
@@ -93,7 +93,14 @@ int64_t CSpell::calculateDamage(const spells::Caster * caster) const
 	if(!isDamage())
 		return 0;
 	auto rawDamage = calculateRawEffectValue(caster->getEffectLevel(this), caster->getEffectPower(this), 1);
+	const std::string cachingStrDamageCap = "type_DAMAGE_RECEIVED_CAP";
+	static const auto selectorDamageCap = Selector::type()(BonusType::DAMAGE_RECEIVED_CAP);
 
+	int damageCapPercentage = info.defender->valOfBonuses(selectorDamageCap, cachingStrDamageCap);
+	if (damageCapPercentage <= 0)
+		return std::numeric_limits<int64_t>::max();
+
+	rawDamage = rawDamage * damageCapPercentage / 100;
 	return caster->getSpellBonus(this, rawDamage, nullptr);
 }
 
@@ -403,6 +410,14 @@ int64_t CSpell::adjustRawDamage(const spells::Caster * caster, const battle::Uni
 		if(affectedCreature->isInvincible())
 			ret = 0;
 	}
+	const std::string cachingStrDamageCap = "type_DAMAGE_RECEIVED_CAP";
+	static const auto selectorDamageCap = Selector::type()(BonusType::DAMAGE_RECEIVED_CAP);
+
+	int damageCapPercentage = info.defender->valOfBonuses(selectorDamageCap, cachingStrDamageCap);
+	if (damageCapPercentage <= 0)
+		return std::numeric_limits<int64_t>::max();
+
+	ret = ret * damageCapPercentage / 100;
 	ret = caster->getSpellBonus(this, ret, affectedCreature);
 	return ret;
 }
