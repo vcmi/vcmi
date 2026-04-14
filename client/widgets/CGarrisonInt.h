b667fbb1c950ec2c8246db3156db7edb47806911
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-14 13:45:41 +0200

    Update CGarrisonInt.h

--- client/widgets/CGarrisonInt.h
+++ client/widgets/CGarrisonInt.h
@@ -39,6 +39,7 @@ class CGarrisonSlot : public CIntObject
 	EGarrisonType upg;
 
 	std::shared_ptr<CAnimImage> creatureImage;
+	std::shared_ptr<CPicture> artifactBackgroundImage;
 	std::shared_ptr<CAnimImage> artifactImage;
 	std::shared_ptr<CAnimImage> selectionImage; // image for selection, not always visible
 	std::shared_ptr<CLabel> stackCount;
