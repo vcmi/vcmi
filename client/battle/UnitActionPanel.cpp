66e6e5069354cb3b1e8b10ee40f5b180911d8539
George King <98261225+GeorgeK1ng@users.noreply.github.com>
2026-04-15 21:09:36 +0200

    Update UnitActionPanel.cpp

--- client/battle/UnitActionPanel.cpp
+++ client/battle/UnitActionPanel.cpp
@@ -124,12 +124,14 @@ void UnitActionPanel::setPossibleActions(const std::vector<PossiblePlayerBattleA
 	static const std::vector actionsGenie = { PossiblePlayerBattleAction::RANDOM_GENIE_SPELL };
 	static const std::vector actionsAttack = { PossiblePlayerBattleAction::ATTACK, PossiblePlayerBattleAction::WALK_AND_ATTACK };
 	static const std::vector actionsReturn = { PossiblePlayerBattleAction::ATTACK_AND_RETURN };
+	static const std::vector actionsAttackNoLongWeapon = { PossiblePlayerBattleAction::ATTACK_WITHOUT_LONG_WEAPON };
 
 	testAndAddAction(newActions, actionsMove, ImagePath::builtin("battle/actionMove"), "vcmi.battle.action.move");
 	testAndAddAction(newActions, actionsReturn, ImagePath::builtin("battle/actionReturn"), "vcmi.battle.action.return");
 	testAndAddAction(newActions, actionsAttack, ImagePath::builtin("battle/actionAttack"), "vcmi.battle.action.attack");
 	testAndAddAction(newActions, actionsShoot, ImagePath::builtin("battle/actionShoot"), "vcmi.battle.action.shoot");
 	testAndAddAction(newActions, actionsGenie, ImagePath::builtin("battle/actionGenie"), "vcmi.battle.action.genie");
+	testAndAddAction(newActions, actionsAttackNoLongWeapon, ImagePath::builtin("battle/actionNoLongWeapon"), "vcmi.battle.action.attackNoLongWeapon");
 
 	std::vector<SpellID> spells;
 
