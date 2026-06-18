b92390496d3d7a86cf2a45473269c54495f9c3be
Andrey Filipenkov <decapitator@ukr.net>
2026-06-18 14:18:12 +0300

    update Conan dependencies

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RELEASE_TAG="2026-06-08"
+RELEASE_TAG="2026-06-27"
 FILENAME="$1.txz"
 DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
 
