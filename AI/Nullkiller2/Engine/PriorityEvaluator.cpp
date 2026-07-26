2518aa00ad9a191f568186954a7effbe5314bd11
Boris Nagaev <bnagaev@gmail.com>
2026-07-26 14:56:55 -0500

    Silence valid exploration point warnings
    Recognize transport objects as strategic exploration points and use
    the normal tile-discovery fallback for other visible objects.

--- AI/Nullkiller2/Engine/PriorityEvaluator.cpp
+++ AI/Nullkiller2/Engine/PriorityEvaluator.cpp
@@ -855,9 +855,11 @@ public:
 			{
 				switch(obj->ID.num)
 				{
+					case Obj::BOAT:
 					case Obj::MONOLITH_ONE_WAY_ENTRANCE:
 					case Obj::MONOLITH_TWO_WAY:
 					case Obj::SUBTERRANEAN_GATE:
+					case Obj::WHIRLPOOL:
 						evaluationContext.explorePriority = 1;
 						break;
 					case Obj::REDWOOD_OBSERVATORY:
@@ -865,7 +867,7 @@ public:
 						evaluationContext.explorePriority = 2;
 						break;
 					default:
-						logAi->warn("ExplorePointEvaluator buildEvaluationContext unknown exploration point %d", obj->ID.num);
+						break;
 				}
 			}
 
