cfb3f8f7ac0d029b7f9728593fd8208c851a4b95
Laserlicht <13953785+Laserlicht@users.noreply.github.com>
2026-08-08 19:39:32 +0200

    [temporary] Build against SDL3 dependencies and disable CI fail-fast

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-RELEASE_TAG="2026-07-06"
+RELEASE_TAG="2026-08-14"
 FILENAME="$1.txz"
-DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
+DOWNLOAD_URL="https://github.com/Laserlicht/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
 
 downloadedFile="$RUNNER_TEMP/$FILENAME"
 curl -Lo "$downloadedFile" "$DOWNLOAD_URL"
