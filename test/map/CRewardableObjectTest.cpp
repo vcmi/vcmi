eadc255fc0a860a4052b081f1af0523e117aaedd
Michał Zaremba <michalza.r@gmail.com>
2026-08-25 13:27:24 +0200

    Make cartographer visits team-global

--- test/map/CRewardableObjectTest.cpp
+++ test/map/CRewardableObjectTest.cpp
@@ -11,8 +11,10 @@
 
 #include "../mock/mock_IGameEventCallback.h"
 #include "../mock/mock_IGameInfoCallback.h"
+#include "../mock/TinyMapGameTest.h"
 
 #include "../../lib/CPlayerState.h"
+#include "../../lib/callback/CCallback.h"
 #include "../../lib/callback/CGameInfoCallback.h"
 #include "../../lib/mapObjects/CRewardableObject.h"
 #include "../../lib/mapObjects/CGHeroInstance.h"
@@ -63,6 +65,10 @@ public:
 
 private:
 	PlayerColor player;
+};
+
+class CartographerTeamVisitTest : public TinyMapGameTest
+{
 };
 
 }
@@ -130,6 +136,83 @@ TEST(CRewardableObject, onceVisitStateIsKnownOnlyToScoutingTeam)
 	EXPECT_TRUE(object.wasVisited(visitor));
 	EXPECT_TRUE(object.wasVisited(ally));
 	EXPECT_FALSE(object.wasVisited(enemy));
+}
+
+TEST(CRewardableObject, globallyVisitedObjectInfoIsScoutedAndUnavailable)
+{
+	const PlayerColor ally(1);
+	IGameInfoCallbackMock objectCallback;
+	PlayerState allyState(&objectCallback);
+	TeamState team;
+	CRewardableObject object(&objectCallback);
+	CGHeroInstance hero(&objectCallback);
+	GameInfoCallbackForTest callback(ally);
+	RewardableObjectInfo info;
+	object.ID = Obj::CARTOGRAPHER;
+	object.subID = MapObjectSubID(1);
+	object.id = ObjectInstanceID(42);
+	object.configuration.visitMode = Rewardable::VISIT_PLAYER_GLOBAL;
+	object.configuration.info.emplace_back().visitType = Rewardable::EEventType::EVENT_FIRST_VISIT;
+	hero.tempOwner = ally;
+	allyState.visitedObjectsGlobal.insert({object.ID, object.subID});
+
+	EXPECT_CALL(objectCallback, getPlayerState(ally, ::testing::_)).WillRepeatedly(::testing::Return(&allyState));
+	EXPECT_CALL(objectCallback, getPlayerTeam(ally)).WillRepeatedly(::testing::Return(&team));
+
+	EXPECT_TRUE(object.wasVisited(ally));
+	EXPECT_TRUE(object.wasScouted(ally));
+	EXPECT_TRUE(callback.getRewardableObjectInfo(&object, info, &hero));
+	EXPECT_TRUE(info.scouted);
+	EXPECT_FALSE(info.rewardAvailable);
+	EXPECT_TRUE(info.rewards.empty());
+
+	object.subID = MapObjectSubID(2);
+	EXPECT_TRUE(callback.getRewardableObjectInfo(&object, info, &hero));
+	EXPECT_FALSE(info.scouted);
+	EXPECT_FALSE(info.rewardAvailable);
+}
+
+TEST_F(CartographerTeamVisitTest, cartographerVisitMarksObjectTypeVisitedForAllies)
+{
+	const PlayerColor visitor(0);
+	const PlayerColor ally(1);
+	const PlayerColor enemy(2);
+	const MapObjectSubID cartographerType(1);
+	startWithMap(TinyH3M::TinyH3MBuilder(EMapFormat::SOD)
+		.size(36, false)
+		.name("CartographerTeamVisit")
+		.playerActive(visitor)
+		.playerActive(ally)
+		.playerActive(enemy)
+		.hero({5, 5, 0}, HeroTypeID(0), visitor)
+		.hero({7, 7, 0}, HeroTypeID(1), ally)
+		.hero({11, 11, 0}, HeroTypeID(2), enemy));
+
+	auto & visitorState = gameState()->players.at(visitor);
+	auto & allyState = gameState()->players.at(ally);
+	auto & enemyState = gameState()->players.at(enemy);
+	const TeamID visitorTeam = visitorState.team;
+	const TeamID allyTeam = allyState.team;
+	allyState.team = visitorTeam;
+	gameState()->teams.at(visitorTeam).players.insert(ally);
+	if(allyTeam != visitorTeam) gameState()->teams.at(allyTeam).players.erase(ally);
+
+	auto callback = makeCallback(visitor);
+	auto cartographer = std::make_shared<CRewardableObject>(callback.get());
+	cartographer->ID = Obj::CARTOGRAPHER;
+	cartographer->subID = cartographerType;
+	cartographer->appearance = findHeroByOwner(visitor)->appearance;
+	cartographer->instanceName = "cartographerForTeamVisitTest";
+	cartographer->setAnchorPos({9, 9, 0});
+	map()->addNewObject(cartographer);
+
+	ChangeObjectVisitors visit(ChangeObjectVisitors::VISITOR_ADD_HERO, cartographer->id, findHeroByOwner(visitor)->id);
+	gameState()->apply(visit);
+
+	EXPECT_EQ(visitorState.visitedObjectsGlobal.count({Obj::CARTOGRAPHER, cartographerType}), 1);
+	EXPECT_EQ(allyState.visitedObjectsGlobal.count({Obj::CARTOGRAPHER, cartographerType}), 1);
+	EXPECT_EQ(enemyState.visitedObjectsGlobal.count({Obj::CARTOGRAPHER, cartographerType}), 0);
+	EXPECT_EQ(allyState.visitedObjectsGlobal.count({Obj::CARTOGRAPHER, MapObjectSubID(2)}), 0);
 }
 
 TEST(CRewardableObject, failedRewardLimiterScoutsWithoutMarkingVisited)
