d530caaf86b81b7106e26ecb5f68165bcb4befff
Michał Zaremba <michalza.r@gmail.com>
2026-08-26 10:54:16 +0200

    Let AI heroes - with full army - free a slot for creature bank rewards

--- AI/Nullkiller2/Engine/PriorityEvaluator.h
+++ AI/Nullkiller2/Engine/PriorityEvaluator.h
@@ -83,6 +83,7 @@ struct DLL_EXPORT EvaluationContext
 	bool isHero;
 	bool isEnemy;
 	bool unusableCreatureBankReward;
+	bool preparesForCreatureBankReward;
 	int explorePriority; // 1 important, 2 medium, 3 lowest importance
 	float powerRatio; // powerRatio = heroPower / totalPower. The ratio of a hero's army strength to the total power of all creatures available to the AI
 
