6a7132202fb06177372106aa9e4e11ae1b6574bc
Ivan Savenko <saven.ivan@gmail.com>
2026-09-13 17:28:44 +0300

    Move string constants into a source file to avoid a copy per translation unit

---
+++ lib/constants/StringConstants.cpp
@@ -0,0 +1,118 @@
+/*
+ * constants/StringConstants.cpp, part of VCMI engine
+ *
+ * Authors: listed in file AUTHORS in main folder
+ *
+ * License: GNU General Public License v2.0 or later
+ * Full text of license available in license.txt file, in main folder
+ *
+ */
+
+#include "StdInc.h"
+
+#include "StringConstants.h"
+
+namespace GameConstants
+{
+	const std::string RESOURCE_NAMES [RESOURCE_QUANTITY] = {
+		"wood", "mercury", "ore", "sulfur", "crystal", "gems", "gold"
+	};
+
+	const std::string PLAYER_COLOR_NAMES [PlayerColor::PLAYER_LIMIT_I] = {
+		"red", "blue", "tan", "green", "orange", "purple", "teal", "pink"
+	};
+
+	const std::string ALIGNMENT_NAMES [4] = {"good", "evil", "neutral", "none"};
+
+	const std::string DIFFICULTY_NAMES [5] = {"pawn", "knight", "rook", "queen", "king"};
+}
+
+namespace NPrimarySkill
+{
+	const std::string names [GameConstants::PRIMARY_SKILLS] = { "attack", "defence", "spellpower", "knowledge" };
+}
+
+namespace NSecondarySkill
+{
+	const std::string names [GameConstants::SKILL_QUANTITY] =
+	{
+		"pathfinding",  "archery",      "logistics",    "scouting",     "diplomacy",    //  5
+		"navigation",   "leadership",   "wisdom",       "mysticism",    "luck",         // 10
+		"ballistics",   "eagleEye",     "necromancy",   "estates",      "fireMagic",    // 15
+		"airMagic",     "waterMagic",   "earthMagic",   "scholar",      "tactics",      // 20
+		"artillery",    "learning",     "offence",      "armorer",      "intelligence", // 25
+		"sorcery",      "resistance",   "firstAid"
+	};
+
+	const std::vector<std::string> levels =
+	{
+		"none", "basic", "advanced", "expert"
+	};
+}
+
+namespace EBuildingType
+{
+	const std::string names [46] =
+	{
+		"mageGuild1",       "mageGuild2",       "mageGuild3",       "mageGuild4",       "mageGuild5",       //  5
+		"tavern",           "shipyard",         "fort",             "citadel",          "castle",           // 10
+		"villageHall",      "townHall",         "cityHall",         "capitol",          "marketplace",      // 15
+		"resourceSilo",     "blacksmith",       "special1",         "horde1",           "horde1Upgr",       // 20
+		"ship",             "special2",         "special3",         "special4",         "horde2",           // 25
+		"horde2Upgr",       "grail",            "extraTownHall",    "extraCityHall",    "extraCapitol",     // 30
+		"dwellingLvl1",     "dwellingLvl2",     "dwellingLvl3",     "dwellingLvl4",     "dwellingLvl5",     // 35
+		"dwellingLvl6",     "dwellingLvl7",     "dwellingUpLvl1",   "dwellingUpLvl2",   "dwellingUpLvl3",   // 40
+		"dwellingUpLvl4",   "dwellingUpLvl5",   "dwellingUpLvl6",   "dwellingUpLvl7",   "dwellingLvl8",
+		"dwellingUpLvl8"
+	};
+}
+
+namespace NFaction
+{
+	const std::string names [GameConstants::F_NUMBER] =
+	{
+		"castle",       "rampart",      "tower",
+		"inferno",      "necropolis",   "dungeon",
+		"stronghold",   "fortress",     "conflux"
+	};
+}
+
+namespace NArtifactPosition
+{
+	const std::string backpack = "backpack";
+}
+
+namespace NPathfindingLayer
+{
+	const std::string names[EPathfindingLayer::NUM_LAYERS] =
+	{
+		"land", "sail", "water", "aviate", "air"
+	};
+}
+
+namespace MappedKeys
+{
+	const std::map<std::string, BuildingSubID::EBuildingSubID> SPECIAL_BUILDINGS =
+	{
+		{ "mysticPond", BuildingSubID::MYSTIC_POND },
+		{ "castleGate", BuildingSubID::CASTLE_GATE },
+		{ "portalOfSummoning", BuildingSubID::PORTAL_OF_SUMMONING },
+		{ "library", BuildingSubID::LIBRARY },
+		{ "treasury", BuildingSubID::TREASURY },
+		{ "bank", BuildingSubID::BANK },
+		{ "auroraBorealis", BuildingSubID::AURORA_BOREALIS }
+	};
+
+	const std::map<std::string, EMarketMode> MARKET_NAMES_TO_TYPES =
+	{
+		{ "resource-resource", EMarketMode::RESOURCE_RESOURCE },
+		{ "resource-player", EMarketMode::RESOURCE_PLAYER },
+		{ "creature-resource", EMarketMode::CREATURE_RESOURCE },
+		{ "resource-artifact", EMarketMode::RESOURCE_ARTIFACT },
+		{ "artifact-resource", EMarketMode::ARTIFACT_RESOURCE },
+		{ "artifact-experience", EMarketMode::ARTIFACT_EXP },
+		{ "creature-experience", EMarketMode::CREATURE_EXP },
+		{ "creature-undead", EMarketMode::CREATURE_UNDEAD },
+		{ "resource-skill", EMarketMode::RESOURCE_SKILL },
+	};
+}
