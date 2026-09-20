ddd3a9c6a50103b60324eb45f05f258fb8faff22
kdmcser <zqtndfj@sina.com>
2026-09-21 00:06:13 +0800

    Unify percentage contribution handling in BonusList

--- lib/bonuses/BonusList.cpp
+++ lib/bonuses/BonusList.cpp
@@ -83,35 +83,35 @@ int BonusList::totalValue(int baseValue) const
 	int indexMaxCount = 0;
 	int indexMinCount = 0;
 
-	std::array<int, vstd::to_underlying(BonusSource::NUM_BONUS_SOURCE)> percentToSource = {};
+	struct PercentContribution
+	{
+		BonusSource source;
+		BonusSourceID sourceID; //empty = applies to the whole source type
+		int value;
+	};
 
-	// PERCENT_TO_TARGET_TYPE bonuses that explicitly name a single source object - they apply only to it
-	std::vector<const Bonus *> percentToSourceID;
+	// Percentage adjustments applied to bonuses depending on the source they come from
+	std::vector<PercentContribution> percentToSource;
 
 	for(const auto & b : bonuses)
 	{
 		switch(b->valType)
 		{
 		case BonusValueType::PERCENT_TO_SOURCE:
-			percentToSource[vstd::to_underlying(b->source)] += b->val;
+			percentToSource.push_back({b->source, BonusSourceID(), b->val});
 		break;
 		case BonusValueType::PERCENT_TO_TARGET_TYPE:
-			if(b->targetSourceID.hasValue())
-				percentToSourceID.push_back(b.get());
-			else
-				percentToSource[vstd::to_underlying(b->targetSourceType)] += b->val;
+			percentToSource.push_back({b->targetSourceType, b->targetSourceID, b->val});
 			break;
 		}
 	}
 
 	for(const auto & b : bonuses)
 	{
-		int sourceIndex = vstd::to_underlying(b->source);
-
-		int percent = percentToSource[sourceIndex];
-		for(const Bonus * restriction : percentToSourceID)
-			if(restriction->targetSourceType == b->source && restriction->targetSourceID == b->sid)
-				percent += restriction->val;
+		int percent = 0;
+		for(const auto & [source, sourceID, value] : percentToSource)
+			if(source == b->source && (sourceID == BonusSourceID() || sourceID == b->sid))
+				percent += value;
 
 		// Workaround: creature hero specialties in H3 is the only place that uses rounding up in bonuses
 		// TODO: try to find more elegant solution?
